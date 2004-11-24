849bc6da5d890b6bcc9dbee7557249d0ae3d86e4
smoutch <smoutch@users.noreply.github.com>
2004-11-24 16:22:19 +0000

    new ctrl implemented in WiredBeatBox Plugin
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk/wired@35 6f5cd6f6-390f-0410-abd1-b47fe60d5253

---
+++ src/libs/WiredWidgets/src/BitmapKnob.h
@@ -0,0 +1,6 @@
+#ifndef __BITMAPKNOB_H__
+#define __BITMAPKNOB_H__
+
+class BitmapKnob : public 
+
+#endif//__BITMAPKNOB_H__
---
+++ src/libs/WiredWidgets/src/StaticPosKnob.h
@@ -0,0 +1,45 @@
+#ifndef __STATICPOSKNOB_H__
+#define __STATICPOSKNOB_H__
+
+#include <wx/wx.h>
+#include <wx/image.h>
+#include <wx/bitmap.h>
+#include "wx/dragimag.h"
+#include <iostream>
+
+using namespace std;
+
+class StaticPosKnob : public wxWindow
+{
+ public:
+  StaticPosKnob(wxWindow *parent, wxWindowID id, 
+		int step, wxImage **img_bg, int mouse_step,
+		int begin_value, int end_value, int init_val,
+		const wxPoint &pos, const wxSize &size);
+  ~StaticPosKnob();
+  
+  virtual void OnPaint(wxPaintEvent &event);
+  virtual void OnMotionEvent(wxMouseEvent &event);
+  virtual void OnLeftUp(wxMouseEvent &event);
+  virtual void OnLeftDown(wxMouseEvent &event);
+  virtual void OnKeyDown(wxKeyEvent& event);
+  
+  int GetValue();
+  void SetValue(int val);
+
+ protected:
+  bool		Clicked;
+  int		Steps;
+  int		MouseStep;
+  int		BeginVal;
+  int		EndVal;
+  int		CurVal;
+  
+  int		ClickPos;
+  int		Count;
+  wxBitmap**	Bmps;
+
+
+DECLARE_EVENT_TABLE()
+};
+#endif//__STATICPOSKNOB_H__
