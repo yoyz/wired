6d8334ff2109a1d0bbbc9b59029e4ec1b7ed4b14
smoutch <smoutch@users.noreply.github.com>
2005-01-11 15:18:45 +0000

    hint integrated to standard KnobCtrl via an other construtor
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@115 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- wired/src/libs/WiredWidgets/src/KnobCtrl.cpp
+++ wired/src/libs/WiredWidgets/src/KnobCtrl.cpp
@@ -6,9 +6,11 @@ BEGIN_EVENT_TABLE(KnobCtrl, wxWindow)
   EVT_PAINT(KnobCtrl::OnPaint)
   EVT_LEFT_DOWN(KnobCtrl::OnMouseEvent)
   EVT_RIGHT_DOWN(KnobCtrl::OnMouseEvent)
-  EVT_LEFT_UP(KnobCtrl::OnMouseEvent)
+  EVT_LEFT_UP(KnobCtrl::OnLeftUp)
   EVT_ENTER_WINDOW(KnobCtrl::OnEnterWindow)
+  EVT_LEAVE_WINDOW(KnobCtrl::OnLeave)
   EVT_KEY_DOWN(KnobCtrl::OnKeyDown)
+  EVT_KEY_UP(KnobCtrl::OnKeyUp)
 END_EVENT_TABLE()
 
 namespace Limits
@@ -19,12 +21,13 @@ namespace Limits
   }
 }
 
-  KnobCtrl::KnobCtrl(wxWindow *parent, wxWindowID id, wxImage *img_bg, wxImage *img_fg,
-		     int begin_value, int end_value, int init_val, int pas,
-		     const wxPoint &pos, const wxSize &size)
-    : wxWindow(parent, id, pos, wxSize(img_bg->GetWidth(), img_bg->GetHeight())) ,
-      knob_fg(img_fg), knob_bg(img_bg), Size(size), Value(0), OldValue(0), 
-      BeginValue(begin_value), EndValue(end_value), Pas(pas), oldy(0)
+KnobCtrl::KnobCtrl(wxWindow *parent, wxWindowID id, wxImage *img_bg, 
+		   wxImage *img_fg, int begin_value, int end_value, 
+		   int init_val, int pas, 
+		   const wxPoint &pos, const wxSize &size)
+  : wxWindow(parent, id, pos, wxSize(img_bg->GetWidth(), img_bg->GetHeight())),
+    knob_fg(img_fg), knob_bg(img_bg), Size(size), Value(0), OldValue(0), 
+    BeginValue(begin_value), EndValue(end_value), Pas(pas), oldy(0)
 {
   assert(Limits::Knob::Higher);
   if (Pas == 0)
@@ -34,7 +37,38 @@ namespace Limits
   fg_tmp->SetMask(fg_mask);
   fg = new wxStaticBitmap(this, -1, *fg_tmp, wxPoint(0,0), wxSize(knob_fg->GetWidth(), knob_fg->GetHeight()));
   SetValue(init_val);
+  Label = 0x0;
 }
+
+KnobCtrl::KnobCtrl(wxWindow *parent, wxWindowID id,
+		   wxImage *img_bg, wxImage *img_fg,
+		   int begin_value, int end_value, int init_val, int pas,
+		   const wxPoint &pos, const wxSize &size, 
+		   wxWindow* hintparent, const wxPoint& hintpos)
+  : wxWindow(parent, id, pos, wxSize(img_bg->GetWidth(), img_bg->GetHeight())),
+    knob_fg(img_fg), knob_bg(img_bg), Size(size), Value(0), OldValue(0), 
+    BeginValue(begin_value), EndValue(end_value), Pas(pas), oldy(0)
+{
+  assert(Limits::Knob::Higher);
+  if (Pas == 0)
+    throw Pas; // FIXME throw something serious
+  fg_tmp = new wxBitmap(knob_fg, -1); 
+  fg_mask = new wxMask(*fg_tmp, *wxBLACK);
+  fg_tmp->SetMask(fg_mask);
+  fg = new wxStaticBitmap(this, -1, *fg_tmp, 
+			  wxPoint(0,0), wxSize(knob_fg->GetWidth(), 
+					       knob_fg->GetHeight()));
+  SetValue(init_val);
+  
+  wxString s;
+  s.Printf("%d", init_val);
+  Label = new Hint(hintparent, -1, s, 
+		   wxPoint( hintpos.x + pos.x + GetSize().x, 
+			    hintpos.y + pos.y + GetSize().y ),
+		   wxDefaultSize, *wxWHITE, *wxBLACK);
+  Label->Show(false);
+}
+
 
 KnobCtrl::~KnobCtrl()
 {
@@ -49,8 +83,16 @@ void KnobCtrl::OnPaint(wxPaintEvent &event)
   dc.Blit(0, 0, knob_bg->GetWidth(), knob_bg->GetHeight(), &memDC, 0, 0, wxCOPY, FALSE);
 }
 
+void KnobCtrl::OnLeftUp(wxMouseEvent& event)
+{
+  if (Label)
+    Label->Show(false);
+  OnMouseEvent(event);
+}
+
 void KnobCtrl::OnMouseEvent(wxMouseEvent &event)
 {
+  
   static bool first = true;
   wxClientDC dc (this);
   wxPoint offset;
@@ -77,11 +119,54 @@ void KnobCtrl::OnMouseEvent(wxMouseEvent &event)
     {
       wxPostEvent(GetParent(), event);
     }
+  if (event.LeftIsDown() && Label)
+    {
+      Label->Show(true);
+      wxString s;
+      s.Printf("%d", GetValue());
+      Label->SetLabel(s);
+    }
+  else if (!event.LeftIsDown() && Label)
+    Label->Show(false);
+
 }
 
 void KnobCtrl::OnKeyDown(wxKeyEvent& event)
 {
+  int value = GetValue();
+  if(event.GetKeyCode() == WXK_UP)
+    {
+      value++;
+      if (value > EndValue)
+	value = EndValue;
+      SetValue(value + 1); // need a fix, SetValue(x) <==> x-1 = GetValue()
+      wxScrollEvent e(wxEVT_SCROLL_TOP, GetId());
+      e.SetEventObject(this);
+      GetEventHandler()->ProcessEvent(e);
+    }
+  else if(event.GetKeyCode() == WXK_DOWN)
+    {
+      value--;
+      if (value < BeginValue)
+	value = BeginValue;
+      SetValue(value);
+      wxScrollEvent e(wxEVT_SCROLL_TOP, GetId());
+      e.SetEventObject(this);
+      GetEventHandler()->ProcessEvent(e);
+    }
+  if (Label)
+    {
+      Label->Show(true);
+      wxString s;
+      s.Printf("%d", GetValue());
+      Label->SetLabel(s);
+    }
+}
 
+void KnobCtrl::OnKeyUp(wxKeyEvent& WXUNUSED(event))
+{
+  if (Label)
+    Label->Show(false);
 }
 
 int KnobCtrl::GetValue()
@@ -91,7 +176,6 @@ int KnobCtrl::GetValue()
 
 void KnobCtrl::SetValue(int val)
 {
-  //  cout << "set value : " << (val - BeginValue) * Limits::Knob::Higher / (EndValue - BeginValue) << endl;
   // FIXME verify result is > 0 and < Limits::Knob::Higher
   SetValue_((val - BeginValue) * Limits::Knob::Higher / (EndValue - BeginValue));
 }
@@ -117,5 +201,12 @@ void KnobCtrl::SetValue_(unsigned val)
 
 void KnobCtrl::OnEnterWindow(wxMouseEvent &event)
 {
+  wxPostEvent(GetParent(), event);
+}
+
+void KnobCtrl::OnLeave(wxMouseEvent &event)
+{
+  if (Label)
+    Label->Show(false);
   wxPostEvent(GetParent(), event);
 }
--- wired/src/libs/WiredWidgets/src/KnobCtrl.h
+++ wired/src/libs/WiredWidgets/src/KnobCtrl.h
@@ -7,6 +7,7 @@
 #include "wx/dragimag.h"
 #include <iostream>
 #include "StaticBitmap.h"
+#include "Hint.h"
 
 using namespace std;
 
@@ -16,16 +17,28 @@ class KnobCtrl : public wxWindow
   KnobCtrl(wxWindow *parent, wxWindowID id, wxImage *img_bg, wxImage *img_fg,
 	    int begin_value, int end_value, int init_val, int pas,
 	    const wxPoint &pos, const wxSize &size);
+  KnobCtrl(wxWindow *parent, wxWindowID id,
+	   wxImage *img_bg, wxImage *img_fg,
+	   int begin_value, int end_value, int init_val, int pas,
+	   const wxPoint &pos, const wxSize &size, 
+	   wxWindow* hintparent, const wxPoint& hintpos);
+  
   ~KnobCtrl();
+  
   virtual void OnPaint(wxPaintEvent &event);
   virtual void OnMouseEvent(wxMouseEvent &event);
+  virtual void OnLeftUp(wxMouseEvent &event);
   virtual void OnEnterWindow(wxMouseEvent &event);
   virtual void OnKeyDown(wxKeyEvent& event);
+  virtual void OnKeyUp(wxKeyEvent& event);
+  virtual void OnLeave(wxMouseEvent &event);
+  
   int GetValue();
   void SetValue(int val);
   void SetValue_(unsigned val);
 
  protected:
+  Hint*			Label;
   wxImage		*knob_fg;
   wxImage		*knob_bg;
   wxBitmap		*fg_tmp;
