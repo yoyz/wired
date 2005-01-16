e0c8b5b2a56d5abb27c50040c0c0579f763f2be9
diodio <diodio@users.noreply.github.com>
2005-01-16 00:41:42 +0000

    ajout de la methode delete
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk/wired@128 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- src/audio/ClipBoard.cpp
+++ src/audio/ClipBoard.cpp
@@ -9,6 +9,7 @@ cClipBoard* cClipBoard::spSingleton = 0;
 cClipBoard::cClipBoard(string filename, bool loadmem, t_opening_mode open_mode) 
 : WaveFile (filename, loadmem, open_mode)
 {
+   sizec = 0;
 }
 
 
@@ -31,6 +32,15 @@ void cClipBoard::Copy (WaveFile& wave, int from, int size_of_copy)
 	cout << "[cClipBoard] - Copy" << endl;
 	cout << "[cClipBoard] - Copy : " << size_of_copy << " frames to copy" << endl;
 	
+
+	if (size_of_copy < 0) 
+	  {
+	    from = from + size_of_copy;
+	    size_of_copy = -size_of_copy;  
+	  }
+	
+	sizec = size_of_copy;
+
 	// buffer destine a recevoir les donnees a copier
 	float * rw_buffer = new float [wave.GetNumberOfChannels() * WAVE_TEMP_SIZE];
 
@@ -68,8 +78,15 @@ void cClipBoard::Cut (WaveFile& wave, int from, int size_of_cut)
 	cout << "[cClipBoard] - Cut " << endl;
 	cout << "[cClipBoard] - Cut : " << size_of_cut << " frames to cut" << endl;
 
+	if (size_of_cut < 0) 
+	  {
+	    from = from + size_of_cut;
+	    size_of_cut = -size_of_cut;  
+	  }
+   
+
 	// Verifie qu'on est en mode read/write
-    if ( wave.GetOpenMode() != rwrite )
+	if ( wave.GetOpenMode() != rwrite )
 		throw cException ("File opened in read only mode");
 
 	// buffer destine a recevoir les donnees a copier
@@ -115,6 +132,8 @@ void cClipBoard::Cut (WaveFile& wave, int from, int size_of_cut)
 
 void cClipBoard::Paste (WaveFile& wave, int to)
 {
+   long		size_paste;
+
   cout << "[cClipBoard] - Paste" << endl;
 
   if ( wave.GetOpenMode() != rwrite )
@@ -123,6 +142,7 @@ void cClipBoard::Paste (WaveFile& wave, int to)
   if ( GetNumberOfFrames() <= 0 )
 	throw cException ("Paste : Nothing to paste");
 	
+  size_paste = sizec;
   cout << "[cClipBoard] - Paste : " << GetNumberOfFrames() << " frames to paste" << endl;
 	
   int size_of_paste = GetNumberOfFrames();
@@ -135,6 +155,7 @@ void cClipBoard::Paste (WaveFile& wave, int to)
 
   // Copier les frames a droite de la position d'insertion ds le fichier temp
   int frames_nbr = 0;
+  temp.SetCurrentPosition(0);
   wave.SetCurrentPosition(to);
   sf_count_t read_frames = wave.ReadFloatF(rw_buffer);
   while (read_frames > 0)
@@ -160,14 +181,18 @@ void cClipBoard::Paste (WaveFile& wave, int to)
   // Copie des frames 'decales'
   temp.SetCurrentPosition();
   read_frames = temp.ReadFloatF(rw_buffer);
-  while (read_frames > 0)
+  while ((read_frames > 0) && (size_paste > 0))
   {
 	wave.WriteFloatF(rw_buffer, read_frames);
+	size_paste -= read_frames;
 	read_frames = temp.ReadFloatF(rw_buffer);
+	if (read_frames > size_paste)
+	  read_frames = size_paste;
   }
 
   // Met a jour le nombre de frames du wave
-  wave.SetNumberOfFrames (wave.GetNumberOfFrames() + size_of_paste);
+   wave.SetNumberOfFrames (wave.GetNumberOfFrames() + sizec);
+ 
 
   // Met a jour le header du wave
   sf_command (wave.GetFilePtr(), SFC_UPDATE_HEADER_NOW, NULL, SF_FALSE) ;
@@ -175,3 +200,54 @@ void cClipBoard::Paste (WaveFile& wave, int to)
   delete[] rw_buffer;
 }
 
+
+void cClipBoard::Delete (WaveFile& wave, int from, int size_of_cut)
+{
+	if (size_of_cut < 0) 
+	{
+	  from = from + size_of_cut;
+	  size_of_cut = -size_of_cut;  
+	}
+   
+	// Verifie qu'on est en mode read/write
+    if ( wave.GetOpenMode() != rwrite )
+		throw cException ("File opened in read only mode");
+
+	// buffer destine a recevoir les donnees a copier
+	float * rw_buffer = new float [wave.GetNumberOfChannels() * WAVE_TEMP_SIZE];
+
+
+	// Decaler les frames vers la gauche
+	sf_count_t frames_to_move = wave.GetNumberOfFrames() - (from + size_of_cut) ;
+	sf_count_t count = ( frames_to_move > WAVE_TEMP_SIZE ) ? WAVE_TEMP_SIZE : frames_to_move;
+
+	int idx = 0;
+	int read_pos = from + size_of_cut;
+	wave.SetCurrentPosition (read_pos);
+	count = wave.ReadFloatF (rw_buffer, count);
+	while (count > 0)
+	{
+		wave.SetCurrentPosition (from + idx);
+		count = wave.WriteFloatF (rw_buffer, count);
+		idx += count;
+		frames_to_move -= count;
+		count = ( frames_to_move > WAVE_TEMP_SIZE ) ? WAVE_TEMP_SIZE : frames_to_move;
+		wave.SetCurrentPosition (read_pos + idx);
+		if (count > 0 ) 
+			count = wave.ReadFloatF (rw_buffer, count);
+	}
+
+	// Tronquer le fichier de facon a supprimer les frames superfus
+	sf_count_t  frames = wave.GetNumberOfFrames() - size_of_cut ;
+	sf_command (wave.GetFilePtr(), SFC_FILE_TRUNCATE, &frames, sizeof (frames)) ;
+
+	// Met a jour le nombre de frames du wave
+	wave.SetNumberOfFrames (frames);
+
+    // Met a jour le header du wave
+	sf_command (wave.GetFilePtr(), SFC_UPDATE_HEADER_NOW, NULL, SF_FALSE) ;
+
+	delete[] rw_buffer;
+	
+	cout << "[cClipBoard] - Delete - end" << endl;
+}
