33f9e846edb887d76e33a23ced8301b783ffc0be
pj-leger <pj-leger@users.noreply.github.com>
2005-04-07 13:18:33 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@249 6f5cd6f6-390f-0410-abd1-b47fe60d5253

---
+++ wired/src/plugins/reverb/ReverbPlug.h
@@ -0,0 +1,94 @@
+// Copyright (C) 2005 by Wired Team
+// Under the GNU General Public License
+
+#ifndef __REVERBPLUG_H__
+#define __REVERBPLUG_H__
+
+#include <math.h>
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+   #include <wx/wx.h>
+#endif
+
+#include "Plugin.h"
+#include "KnobCtrl.h"
+#include "FaderCtrl.h"
+
+#include "NRev.h"
+#include "JCRev.h"
+#include "PRCRev.h"
+
+#define PLUGIN_NAME	"Reverb"
+
+#define IMG_RV_BG	"plugins/reverb/reverb.png"
+#define IMG_RV_BMP	"plugins/reverb/ReverbPlug.bmp"
+#define IMG_RV_FADER_BG	"plugins/filter/fader_bg.png"
+#define IMG_RV_FADER_FG	"plugins/filter/fader_fg.png"
+#define IMG_RV_KNOB_BG	"plugins/reverb/knob_bg.png"
+#define IMG_RV_KNOB_FG	"plugins/reverb/knob_fg.png"
+#define EFFECT_MIX	100.f
+
+typedef struct s_param
+{
+  int		sel_rev;
+  float		Mix;
+  float		Decay;
+}		t_param;
+
+class ReverbPlugin: public Plugin
+{
+ public:
+  ReverbPlugin(PlugStartInfo &startinfo, PlugInitInfo *initinfo);
+  ~ReverbPlugin();
+
+  void	 Init();
+
+  void	 Process(float **input, float **output, long sample_length);
+  void	 CreateGui(wxWindow *rack, wxPoint &pos, wxSize &size);
+  void   Load(int fd, long size);
+  long   Save(int fd);
+
+  bool	 IsAudio();
+  bool	 IsMidi();
+
+  std::string DefaultName() { return "Reverb"; }
+
+  void OnSelrev(wxScrollEvent &e);
+  void OnDecay(wxScrollEvent &e);
+  void OnMix(wxScrollEvent &e);  
+
+  void OnPaint(wxPaintEvent &event);
+
+  wxBitmap	*GetBitmap();
+  float		Wide;
+  
+ protected:
+  PRCRev    PRCreverb_stk;
+  JCRev     JCreverb_stk;
+  NRev	    Nreverb_stk;
+  int       rev_sel;
+  
+  t_param param;
+
+  wxBitmap *bmp;   
+
+  FaderCtrl *SelrevKnob;
+  FaderCtrl *DecayKnob;
+  FaderCtrl *MixKnob;
+ 
+  wxImage *img_fg;
+  wxImage *img_bg;
+  wxBitmap *TpBmp;
+
+  DECLARE_EVENT_TABLE()  
+
+};
+
+enum
+  {
+    Reverb_Selrev = 1,
+    Reverb_Decay,
+    Reverb_Mix
+  };
+
+#endif
