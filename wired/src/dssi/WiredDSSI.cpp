c52e01bc51406ccd0e43b98c1ecddb44703ba08a
pj-leger <pj-leger@users.noreply.github.com>
2005-05-12 18:16:31 +0000

    temporary commit for wired team
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@316 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- wired/src/dssi/WiredDSSI.cpp
+++ wired/src/dssi/WiredDSSI.cpp
@@ -1,9 +1,11 @@
-#include "WiredDSSI.h"
+	#include "WiredDSSI.h"
 
 WiredLADSPAInstance::WiredLADSPAInstance()
 {
 	_Handle = NULL;
 	_Descriptor = NULL;
+	_Type = 0;
+	_Properties = 0;
 }
 
 WiredLADSPAInstance::~WiredLADSPAInstance()
@@ -12,7 +14,7 @@ WiredLADSPAInstance::~WiredLADSPAInstance()
 	_Descriptor = NULL;
 }
 
-WiredLADSPAInstance::WiredLADSPAInstance(const WiredLADSPAInstance& copy)
+WiredLADSPAInstance::WiredLADSPAInstance(const WiredLADSPAInstance& copy) : Plugin()
 {
 	*this = copy;
 }
@@ -21,8 +23,15 @@ WiredLADSPAInstance		WiredLADSPAInstance::operator=(const WiredLADSPAInstance& r
 {
 	if (this != &right)
 	{
+		_InputAudioPluginsPorts = right._InputAudioPluginsPorts;
+		_OutputAudioPluginsPorts = right._OutputAudioPluginsPorts;
+		_InputDataPluginsPorts = right._InputDataPluginsPorts;
+		_OutputDataPluginsPorts = right._OutputDataPluginsPorts;
 		_Handle = right._Handle;
 		_Descriptor = right._Descriptor;
+		_Type = right._Type;
+		_Properties = right._Properties;
+		_GuiControls = right._GuiControls;
 	}
 	return *this;
 }
@@ -42,8 +51,15 @@ bool					WiredLADSPAInstance::Load()
 	if (_Descriptor)
 	{
 		UnLoad();
-		if ((_Handle = _Descriptor->instantiate(_Descriptor, Audio->OutputSampleFormat)))
-			return true;
+		if (_Descriptor->instantiate!= NULL)
+			if ((_Handle = _Descriptor->instantiate(_Descriptor, Audio->OutputSampleFormat)))
+			{
+				_Properties = _Descriptor->Properties;
+				Name = _Descriptor->Name;
+				LoadPorts();
+				return true;
+			}
+		cout << "Load Failed" << endl;
 	}
 	return false;
 }
@@ -52,26 +68,370 @@ void					WiredLADSPAInstance::UnLoad()
 {
 	if (_Handle && _Descriptor)
 	{
-		cout << "Unloading Plugin " << _Descriptor->Name << ".";
+		cout << "Unloading Plugin " << _Descriptor->Name;
+		ChangeActivateState(false);
+		cout << ".";
 		_Descriptor->cleanup(_Handle);
 		cout << ".";
 		_Handle = NULL;
+		UnLoadPorts();
 		cout << ".";
 		cout << "done" << endl;
 	}
+}
+
+void					WiredLADSPAInstance::UnloadGUIPorts()
+{
+	map<unsigned long, t_gui_port>::iterator	Iter;
+	
+	for (Iter = _GuiControls.begin(); Iter != _GuiControls.end(); Iter++)
+		if (Iter->second.Data != NULL)
+			delete Iter->second.Data;
 }
 
 bool					WiredLADSPAInstance::ChangeActivateState(bool Activate)
 {
 	if (_Handle && _Descriptor)
 	{
-		if (Activate)
+		if (Activate && _Descriptor->activate != NULL)
 			_Descriptor->activate(_Handle);
-		else
+		else if (_Descriptor->deactivate != NULL)
 			_Descriptor->deactivate(_Handle);
 		return true;
 	}
 	return false;
+}
+
+void					WiredLADSPAInstance::UnLoadPorts()
+{
+	_InputAudioPluginsPorts.clear();
+	_OutputAudioPluginsPorts.clear();
+	_InputDataPluginsPorts.clear();
+	_OutputDataPluginsPorts.clear();	
+}
+
+void					WiredLADSPAInstance::LoadPorts()
+{
+	unsigned long		pos;
+	t_ladspa_port		CurrentPort;
+
+	UnLoadPorts();	
+	for (pos = 0, CurrentPort.Descriptor = 0, CurrentPort.Id = 0, CurrentPort.RangeHint.LowerBound = 0, 
+		 CurrentPort.RangeHint.UpperBound = 0, CurrentPort.RangeHint.HintDescriptor = 0;
+		 pos < _Descriptor->PortCount; 
+	 	 pos ++, CurrentPort.Descriptor = 0, CurrentPort.Id = 0, CurrentPort.RangeHint.LowerBound = 0, 
+		 CurrentPort.RangeHint.UpperBound = 0, CurrentPort.RangeHint.HintDescriptor = 0)
+	{
+		CurrentPort.Descriptor = _Descriptor->PortDescriptors[pos];
+		CurrentPort.RangeHint = _Descriptor->PortRangeHints[pos];
+		CurrentPort.Id = pos;
+		CurrentPort.Name = _Descriptor->PortNames[pos];
+		if (LADSPA_IS_PORT_INPUT(CurrentPort.Descriptor))
+		{
+			if (LADSPA_IS_PORT_CONTROL(CurrentPort.Descriptor))
+			{
+				_InputDataPluginsPorts.insert(_InputDataPluginsPorts.end(), CurrentPort);
+				AddGuiControl(&CurrentPort);
+			}
+			else if (LADSPA_IS_PORT_AUDIO(CurrentPort.Descriptor))
+			{
+				_InputAudioPluginsPorts.insert(_InputAudioPluginsPorts.end(), CurrentPort);
+				_Type |= TYPE_PLUGINS_EFFECT;
+			}
+		}
+		else if (LADSPA_IS_PORT_OUTPUT(CurrentPort.Descriptor))
+		{
+			if (LADSPA_IS_PORT_CONTROL(CurrentPort.Descriptor))
+				_OutputDataPluginsPorts.insert(_OutputDataPluginsPorts.end(), CurrentPort);
+			else if (LADSPA_IS_PORT_AUDIO(CurrentPort.Descriptor))
+				_OutputAudioPluginsPorts.insert(_OutputAudioPluginsPorts.end(), CurrentPort);
+		}
+	}
+	if (!(_Type & TYPE_PLUGINS_EFFECT))
+		_Type |= TYPE_PLUGINS_INSTR;
+	DumpPorts();
+}
+
+void					WiredLADSPAInstance::AddGuiControl(t_ladspa_port *PortData)
+{
+	t_gui_port			NewGuiPort;
+
+	NewGuiPort.LowerBound = PortData->RangeHint.LowerBound;
+	NewGuiPort.UpperBound = PortData->RangeHint.UpperBound;
+	NewGuiPort.Data = new LADSPA_Data;
+	*(NewGuiPort.Data) = GetDefaultValue(&NewGuiPort, PortData->RangeHint.HintDescriptor);
+
+	_GuiControls[PortData->Id] = NewGuiPort;
+}
+
+LADSPA_Data				WiredLADSPAInstance::GetDefaultValue(t_gui_port *GuiPort, LADSPA_PortRangeHintDescriptor Descriptor)
+{
+	if (LADSPA_IS_HINT_HAS_DEFAULT(Descriptor))
+	{
+		if(LADSPA_IS_HINT_DEFAULT_MINIMUM(Descriptor))
+			return GuiPort->LowerBound;
+		else if (LADSPA_IS_HINT_DEFAULT_LOW(Descriptor))
+		{
+			if (LADSPA_IS_HINT_LOGARITHMIC(Descriptor))
+				return (LADSPA_Data) expf(logf(GuiPort->LowerBound) * 0.75 + logf(GuiPort->UpperBound) * 0.25);
+			else
+				return (LADSPA_Data) GuiPort->LowerBound * 0.75 + GuiPort->UpperBound * 0.25;
+		}
+		else if (LADSPA_IS_HINT_DEFAULT_MIDDLE(Descriptor))
+		{
+			if (LADSPA_IS_HINT_LOGARITHMIC(Descriptor))
+				return (LADSPA_Data) expf(logf(GuiPort->LowerBound) * 0.5 + logf(GuiPort->UpperBound) * 0.5);
+			else
+				return (LADSPA_Data) GuiPort->LowerBound * 0.5 + GuiPort->UpperBound * 0.5;
+		}
+		else if (LADSPA_IS_HINT_DEFAULT_HIGH(Descriptor))
+		{
+			if (LADSPA_IS_HINT_LOGARITHMIC(Descriptor))
+				return (LADSPA_Data) expf(logf(GuiPort->LowerBound) * 0.25 + logf(GuiPort->UpperBound) * 0.75);
+			else
+				return (LADSPA_Data) GuiPort->LowerBound * 0.25 + GuiPort->UpperBound * 0.75;
+		}
+		else if (LADSPA_IS_HINT_DEFAULT_MAXIMUM(Descriptor))
+			return GuiPort->UpperBound;
+		else if (LADSPA_IS_HINT_DEFAULT_0(Descriptor))
+			return 0.0f;
+		else if (LADSPA_IS_HINT_DEFAULT_1(Descriptor))
+			return 1.0f;
+		else if (LADSPA_IS_HINT_DEFAULT_100(Descriptor))
+			return 100.0f;
+		else if (LADSPA_IS_HINT_DEFAULT_440(Descriptor))
+			return 440.0f;
+	}
+	return GuiPort->LowerBound;
+}
+
+void					WiredLADSPAInstance::DumpPorts()
+{
+	list<t_ladspa_port>::iterator	Iter;
+	
+	cout << "############## Begin of Ports Dump ##############" << endl;
+	cout << "Input(s) audio" << endl;
+	for (Iter = _InputAudioPluginsPorts.begin(); Iter != _InputAudioPluginsPorts.end(); Iter++)
+		cout << "Input Named {" << Iter->Name.c_str() << "}" << endl;
+	cout << "Input(s) data" << endl;
+	for (Iter = _InputDataPluginsPorts.begin(); Iter != _InputDataPluginsPorts.end(); Iter++)
+		cout << "Input Named {" << Iter->Name.c_str() << "}" << endl;
+	cout << "Output(s) audio" << endl;
+	for (Iter = _OutputAudioPluginsPorts.begin(); Iter != _OutputAudioPluginsPorts.end(); Iter++)
+		cout << "Output Named {" << Iter->Name.c_str() << "}" << endl;
+	cout << "Output(s) data" << endl;
+	for (Iter = _OutputDataPluginsPorts.begin(); Iter != _OutputDataPluginsPorts.end(); Iter++)
+		cout << "Output Named {" << Iter->Name.c_str() << "}" << endl;
+	cout << "############## End of Ports Dump ##############" << endl;
+}
+
+bool					WiredLADSPAInstance::IsLoaded()
+{
+	if (_Handle && _Descriptor)
+		return true;
+	return false;
+}
+
+void	 				WiredLADSPAInstance::Process(float **input, float **output, long sample_length)
+{
+	if (IsLoaded() == false)
+		return;
+	if (_InputAudioPluginsPorts.empty() || _OutputAudioPluginsPorts.empty())
+		return;
+	if (LADSPA_IS_INPLACE_BROKEN(_Properties) && *input == *output)
+		return;
+	ChangeActivateState();
+	if (_InputAudioPluginsPorts.size() >= 2 && _OutputAudioPluginsPorts.size() >= 2)
+		ProcessStereo(input, output, sample_length);
+	else
+		ProcessMono(input, output, sample_length);
+}
+
+void					WiredLADSPAInstance::ConnectMonoInput(float *input, unsigned long PortId)
+{
+	if (IsLoaded())
+		_Descriptor->connect_port(_Handle, PortId, input);
+}
+
+void					WiredLADSPAInstance::ConnectMonoOutput(float *output, unsigned long PortId)
+{
+	if (IsLoaded())
+		_Descriptor->connect_port(_Handle, PortId, output);
+}
+
+void					WiredLADSPAInstance::ProcessStereo(float **input, float **output, long sample_length)
+{
+	if (IsLoaded() && _InputAudioPluginsPorts.size() >= 2 && _OutputAudioPluginsPorts.size() >= 2)
+	{
+		ConnectMonoInput(input[0], GetPortId(_InputAudioPluginsPorts, 0));
+		ConnectMonoOutput(output[0], GetPortId(_OutputAudioPluginsPorts, 0));
+		_Descriptor->run(_Handle, (unsigned long) sample_length);
+		ConnectMonoInput(input[1], GetPortId(_InputAudioPluginsPorts, 1));
+		ConnectMonoOutput(output[1], GetPortId(_OutputAudioPluginsPorts, 1));
+		_Descriptor->run(_Handle, (unsigned long) sample_length);
+	}
+}
+
+void					WiredLADSPAInstance::ProcessMono(float **input, float **output, long sample_length)
+{
+	if (IsLoaded() && _InputAudioPluginsPorts.size() >= 1 && _OutputAudioPluginsPorts.size() >= 1)
+	{
+		ConnectMonoInput(input[0], GetPortId(_InputAudioPluginsPorts, 0));
+		ConnectMonoOutput(output[0], GetPortId(_OutputAudioPluginsPorts, 0));
+		_Descriptor->run(_Handle, (unsigned long) sample_length);
+		ConnectMonoInput(input[1], GetPortId(_InputAudioPluginsPorts, 0));
+		ConnectMonoOutput(output[1], GetPortId(_OutputAudioPluginsPorts, 0));
+		_Descriptor->run(_Handle, (unsigned long) sample_length);
+	}
+}
+
+void					WiredLADSPAInstance::Init()
+{
+	
+}
+
+void					WiredLADSPAInstance::Play()
+{
+	
+}
+
+void					WiredLADSPAInstance::Stop()
+{
+	
+}
+
+unsigned long			WiredLADSPAInstance::GetPortId(list<t_ladspa_port>& PortList, int index)
+{
+	list<t_ladspa_port>::iterator	Iter;
+	unsigned long		Result = 0;
+	int					pos;
+	
+	for (Iter = PortList.begin(), pos = 0; Iter != PortList.end(); Iter++, pos++)
+		if (pos == index)
+		{
+			Result = Iter->Id;
+			break;
+		}
+	return Result;
+}
+
+void					WiredLADSPAInstance::Load(WiredPluginData& Datas)
+{
+	string				Buffer;
+	list<t_ladspa_port>::iterator	Iter;
+	t_gui_port			NewData;
+	
+	for (Iter = _InputDataPluginsPorts.begin(); Iter != _InputDataPluginsPorts.end(); Iter++)
+	{
+		if (_GuiControls.find(Iter->Id) != _GuiControls.end())
+		{
+			Buffer = Datas.LoadValue(Iter->Name);
+			if (Buffer.size() > 0 &&  NewData.Data != NULL)
+				*(NewData.Data) = (LADSPA_Data)strtof(Buffer.c_str(), NULL);
+		}
+	}
+}
+
+void					WiredLADSPAInstance::Save(WiredPluginData& Datas)
+{
+	ostringstream					Buffer;
+	list<t_ladspa_port>::iterator	Iter;
+	
+	for (Iter = _InputDataPluginsPorts.begin(); Iter != _InputDataPluginsPorts.end(); Iter++)
+	{
+		if (_GuiControls.find(Iter->Id) != _GuiControls.end())
+		{
+			Buffer << *(_GuiControls[Iter->Id].Data);
+			Datas.SaveValue(Iter->Name, Buffer.str());
+			Buffer.clear();	
+		}
+	}
+}
+
+void					WiredLADSPAInstance::SetBufferSize(long size)
+{
+	
+}
+
+void					WiredLADSPAInstance::SetSamplingRate(double rate)
+{
+	
+}
+
+void			  		WiredLADSPAInstance::SetBPM(float bpm)
+{
+	
+}
+
+void					WiredLADSPAInstance::SetSignature(int numerator, int denominator)
+{
+	
+}
+	
+void					WiredLADSPAInstance::ProcessEvent(WiredEvent &event)
+{
+	
+}
+
+bool					WiredLADSPAInstance::HasView()
+{
+	
+}
+
+wxWindow*				WiredLADSPAInstance::CreateView(wxWindow *zone, wxPoint &pos, wxSize &size)
+{
+	
+}
+
+void					WiredLADSPAInstance::DestroyView()
+{
+	
+}
+
+bool					WiredLADSPAInstance::IsAudio()
+{
+	if (_Type & TYPE_PLUGINS_EFFECT)
+		return true;
+	return false;
+}
+
+bool					WiredLADSPAInstance::IsMidi()
+{
+	if (_Type & TYPE_PLUGINS_INSTR)
+		return true;
+	return false;
+}
+
+void					WiredLADSPAInstance::AskUpdate()
+{
+	
+}
+
+void					WiredLADSPAInstance::Update()
+{
+	
+}
+
+std::string				WiredLADSPAInstance::GetHelpString()
+{
+	return STR_DEFAULT_HELP;
+}
+
+void					WiredLADSPAInstance::SetHelpMode(bool On)
+{
+	
+}
+
+std::string				WiredLADSPAInstance::DefaultName()
+{
+	if (IsLoaded())
+		return _Descriptor->Name;
+	return STR_DEFAULT_NAME;
+}
+
+wxBitmap*				WiredLADSPAInstance::GetBitmap()
+{
+	
 }
 
 WiredDSSIPlugin::WiredDSSIPlugin()
@@ -248,8 +608,38 @@ bool				WiredDSSIPlugin::CreatePlugin(int PluginId, WiredLADSPAInstance* Plugin)
 		Descriptor = _DSSIDescriptors.find(PluginId)->second->LADSPA_Plugin;
 	cout << "Creating Rack for Plugin " << Descriptor->Name << endl;
 	Plugin->Init(Descriptor);
+	cout << "Rack Created, loading" << endl;
 	return Plugin->Load();
 	return false;
+}
+
+bool				WiredDSSIPlugin::Contains(unsigned long PluginUniqueId)
+{
+	bool			Result = false;
+	
+	if (_DSSIDescriptorFunction)
+	{
+		map<int, const DSSI_Descriptor*>::iterator	Iter;
+
+		for (Iter = _DSSIDescriptors.begin(); Iter != _DSSIDescriptors.end(); Iter++)
+			if (Iter->second->LADSPA_Plugin->UniqueID == PluginUniqueId)
+			{
+				Result = true;
+				break;
+			}
+	}
+	else if (_LADSPADescriptorFunction)
+	{
+		map<int, const LADSPA_Descriptor*>::iterator	Iter;
+
+		for (Iter = _LADSPADescriptors.begin(); Iter != _LADSPADescriptors.end(); Iter++)
+			if (Iter->second->UniqueID == PluginUniqueId)
+			{
+				Result = true;
+				break;
+			}
+	}
+	return Result;
 }
 
 WiredDSSI::WiredDSSI()
@@ -266,7 +656,7 @@ WiredDSSI::~WiredDSSI()
 {
 	list<WiredLADSPAInstance*>::iterator	IterLoaded;
 	WiredLADSPAInstance*					CurrentLoadedPlugin;
-	
+
 	for (IterLoaded = _LoadedPlugins.begin(); IterLoaded != _LoadedPlugins.end(); IterLoaded++)
 	{
 		CurrentLoadedPlugin = *IterLoaded;
@@ -301,11 +691,19 @@ void			WiredDSSI::LoadPLugins(int Type)
 	char			*Dirs = NULL;
 	
 	if (Type & TYPE_PLUGINS_DSSI)
+	{
 		if ((Dirs = getenv(ENV_NAME_PLUGINS_DSSI)) != NULL)
 			LoadPluginsFromPath(Dirs, TYPE_PLUGINS_DSSI);
+		else
+			LoadPluginsFromPath(DEFAULT_DSSI_PATH, TYPE_PLUGINS_DSSI);
+	}
 	if (Type & TYPE_PLUGINS_LADSPA)
+	{
 		if ((Dirs = getenv(ENV_NAME_PLUGINS_LADSPA)) != NULL)
 			LoadPluginsFromPath(Dirs, TYPE_PLUGINS_LADSPA);
+		else
+			LoadPluginsFromPath(DEFAULT_LADSPA_PATH, TYPE_PLUGINS_LADSPA);
+	}
 }
 
 void			WiredDSSI::LoadPluginsFromPath(const char *Dirs, int Type)
@@ -346,7 +744,7 @@ void			WiredDSSI::LoadPlugins(const string& FileName)
 
 list<string>	WiredDSSI::SplitPath(string& Path)
 {
-	ifstream f(Path.c_str());
+	istringstream f(Path.c_str());
 	list<string> Result;
 	string		Buffer;
 
@@ -355,7 +753,6 @@ list<string>	WiredDSSI::SplitPath(string& Path)
 	else
 		while (getline(f, Buffer, ENV_PATH_SEPARATOR))
 			Result.insert(Result.end(), Buffer);
-	f.close();
 	return Result;
 }
 
@@ -392,6 +789,7 @@ int					WiredDSSI::GetPluginType(int PluginId)
 	}
 	return Result;
 }
+
 void				WiredDSSI::CreatePlugin(int MenuItemId)
 {
 	list<WiredDSSIPlugin*>::iterator	Iter;
--- wired/src/dssi/WiredDSSI.h
+++ wired/src/dssi/WiredDSSI.h
@@ -4,17 +4,18 @@
 #include "dssi.h"
 #include "ladspa.h"
 #include "../engine/AudioEngine.h"
+#include "../redist/Plugin.h"
 #include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <dlfcn.h>
+#include <math.h>
 
 #include <list>
 #include <map>
 #include <string>
-#include <fstream>
+#include <sstream>
 #include <iostream>
-
 using namespace std;
 
 #define	TYPE_PLUGINS_DSSI 1
@@ -25,11 +26,36 @@ using namespace std;
 
 #define ENV_NAME_PLUGINS_DSSI "DSSI_PATH"
 #define ENV_NAME_PLUGINS_LADSPA "LADSPA_PATH"
+#define DEFAULT_DSSI_PATH "/usr/lib/dssi:/usr/local/lib/dssi"
+#define DEFAULT_LADSPA_PATH "/usr/lib/ladspa:/usr/local/lib/ladspa"
 #define ENV_PATH_SEPARATOR ':'
 #define STR_DSSI_DESCRIPTOR_FUNCTION_NAME "dssi_descriptor"
 #define STR_LADSPA_DESCRIPTOR_FUNCTION_NAME "ladspa_descriptor"
+#define STR_DEFAULT_NAME "LADSPA Plugin"
+#define STR_DEFAULT_HELP "No help provided by this plugin"
 
-class	WiredLADSPAInstance
+typedef struct s_gui_port
+{
+	LADSPA_Data		LowerBound;
+	LADSPA_Data		UpperBound;
+	LADSPA_Data		*Data;
+} t_gui_port;
+
+typedef struct s_ladspa_port
+{
+	LADSPA_PortDescriptor	Descriptor;
+	LADSPA_PortRangeHint	RangeHint;
+	unsigned long			Id;	
+	string					Name;
+} t_ladspa_port;
+
+typedef struct s_gui_control
+{
+	t_gui_port		*Data;
+	t_ladspa_port	*Descriptor;
+} t_gui_control;
+
+class	WiredLADSPAInstance : public Plugin
 {
 public:
 	WiredLADSPAInstance();
@@ -39,11 +65,57 @@ public:
 	bool				Init(const LADSPA_Descriptor* Descriptor);
 	bool				Load();
 	bool				ChangeActivateState(bool Activate = true);
+
+	//<Wired Plugin Implementation>
+	void	 			Process(float **input, float **output, long sample_length);
+	void				Init();
+	void				Play();
+	void				Stop();
+	void				Load(WiredPluginData& Datas);
+	void				Save(WiredPluginData& Datas);
+	void				SetBufferSize(long size);
+	void				SetSamplingRate(double rate);
+	void			  	SetBPM(float bpm);
+	void				SetSignature(int numerator, int denominator);
+	void				ProcessEvent(WiredEvent &event);
+	bool				HasView();
+	wxWindow			*CreateView(wxWindow *zone, wxPoint &pos, wxSize &size);
+	void				DestroyView();
+	bool				IsAudio();
+	bool				IsMidi();
+	void				AskUpdate();
+	void				Update();
+	std::string			GetHelpString();
+	void				SetHelpMode(bool On);
+	std::string			DefaultName();
+	wxBitmap			*GetBitmap();	
+	//</Wired Plugin Implementation>
+	
 private:
 	void				UnLoad();
-	
-	LADSPA_Handle		_Handle;
-	LADSPA_Descriptor	*_Descriptor;
+	void				LoadPorts();
+	void				UnLoadPorts();
+	void				DumpPorts();
+	bool				IsLoaded();
+	void				ConnectMonoInput(float *input, unsigned long PortId);
+	void				ConnectMonoOutput(float *output, unsigned long PortId);
+	void				ProcessStereo(float **input, float **output, long sample_length);
+	void				ProcessMono(float **input, float **output, long sample_length);
+	unsigned long		GetPortId(list<t_ladspa_port>& PortList, int index);
+	void				SetDataPortDefaultValue(t_ladspa_port *PortData);
+	LADSPA_Data			GetDefaultValue(t_gui_port *GuiPort, LADSPA_PortRangeHintDescriptor Descriptor);
+	void				AddGuiControl(t_ladspa_port *PortData);
+	void				UnloadGUIPorts();
+
+	int								_Type;
+	LADSPA_Handle					_Handle;
+	LADSPA_Descriptor				*_Descriptor;
+	LADSPA_Properties				_Properties;
+	list<t_ladspa_port>				_InputAudioPluginsPorts;
+	list<t_ladspa_port>				_OutputAudioPluginsPorts;
+	list<t_ladspa_port>				_InputDataPluginsPorts;
+	list<t_ladspa_port>				_OutputDataPluginsPorts;
+	map<unsigned long, t_gui_port>	_GuiControls;								//Key == PortId; Value == PortData
 };
 
 class	WiredDSSIPlugin
@@ -59,14 +131,15 @@ public:
 	int					GetPluginType(int PluginId);
 	bool				Contains(int PluginId);
 	bool				CreatePlugin(int PluginId, WiredLADSPAInstance *Plugin);
+	bool				Contains(unsigned long PluginUniqueId);
 
 private:
 	string								_FileName;
 	void								*_Handle;
 	DSSI_Descriptor_Function			_DSSIDescriptorFunction;
-	map<int, const DSSI_Descriptor*>	_DSSIDescriptors;
+	map<int, const DSSI_Descriptor*>	_DSSIDescriptors;						//Key == PluginId; Value == PluginDescriptor
 	LADSPA_Descriptor_Function			_LADSPADescriptorFunction;
-	map<int, const LADSPA_Descriptor*>	_LADSPADescriptors;
+	map<int, const LADSPA_Descriptor*>	_LADSPADescriptors;						//Key == PluginId; Value == PluginDescriptor
 	map<int, int>						_PluginsInfo;
 };
 
@@ -91,7 +164,7 @@ private:
 
 	list<WiredDSSIPlugin*>		_Plugins;
 	list<WiredLADSPAInstance*>	_LoadedPlugins;
-	map<int, int>			_IdTable;
+	map<int, int>			_IdTable;									//Key == MenuItemId; Value == PluginId (auto-increment)
 	int						_CurrentPluginIndex;
 };
 
