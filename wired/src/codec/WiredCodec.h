35b4e333df2cd6baffa2bcf75df4c7fef2cad3d3
mono-g <mono-g@users.noreply.github.com>
2005-07-06 20:54:24 +0000

    Encode and Decode compressed files
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@357 6f5cd6f6-390f-0410-abd1-b47fe60d5253

---
+++ wired/src/codec/WiredCodec.h
@@ -0,0 +1,69 @@
+#ifndef __WiredCodec_H__
+#define __WiredCodec_H__
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+#include <dlfcn.h>
+
+#include <vector>
+#include <list>
+#include <string>
+#include <iostream>
+#include "./wiredcodec/WiredApiCodec.h"
+
+using namespace std;
+
+#define CODEC_PATH	"/usr/local/lib"
+
+#define	WLIBCONSTRUCT	"WiredCodecConstruct"
+
+#define BUF_SIZE	4049
+
+class			WiredCodec
+{
+ public:
+  WiredCodec();
+  WiredCodec(const string& DataPath);
+  ~WiredCodec();
+
+  void			Init();
+  t_Pcm      		Decode(const string filename);
+  int			Encode(float **pcm, string OutExtension);
+
+  /*Return a list of extension readable by all codecs*/
+  list<string>		GetExtension();
+
+ private:
+
+  /*look for .so*/
+  void			InitWLib();
+
+  /*load all .so*/
+  void			WLoadLib();
+
+  /*load .so*/
+  void			WLibLoader(const string& filename);
+
+  /*check extension to don t have same occurence two time*/
+  int			CheckExtension(const string& str);
+
+  /*list of extension*/
+  list<string>	_ExtList;
+
+  /*Instance of codec found*/
+  list<t_WLib>		_WLib;
+
+  /*path of codecs*/
+  string		_WiredPath;
+
+  /*list of codec.so*/
+  vector<string>	_WiredSo;
+
+  t_WLib WiredCodec::FindBestCodec(string extension);
+};
+
+typedef WiredApiCodec* (*WiredCodecConstruct)();
+
+#endif
