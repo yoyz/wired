ec47d284c31102393b5c039c3405d5dbb89784e1
mono-g <mono-g@users.noreply.github.com>
2005-07-06 20:55:57 +0000

    API to use to encode and decode compressed file
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@358 6f5cd6f6-390f-0410-abd1-b47fe60d5253

---
+++ wired/src/codec/wiredcodec/WiredApiCodec.h
@@ -0,0 +1,76 @@
+#ifndef __WiredAPICodec_H__
+#define __WiredAPICodec_H__
+
+#include <stdlib.h>
+#include <list>
+#include <string>
+
+#define	EXIST	0x0
+#define	DECODE	0x1
+#define	ENCODE	0x2
+
+#define	CODEC_MASK	0x0
+
+#define USE_FILENAME	1
+#define USE_PCM		2
+
+using namespace std;
+
+typedef struct		s_LibInfo
+{
+  /*what can do the lib*/
+  int			CodecMask;
+  /*extension readable by the lib*/
+  string	       	Extension;
+  /*Note to determine if that codec is the best for this extension*/
+  short			Note;
+}			t_LibInfo;
+
+enum PcmType
+  {
+	UInt8 = 0,
+	Int8,
+	Int16,
+	Int24,
+	Float32
+  };
+
+
+typedef struct   s_Pcm
+{
+  void			*pcm;
+  PcmType		PType;
+  int			TotalSample;
+  int			SampleRate;
+  int			Channels;
+}		 t_Pcm;
+
+
+
+class   WiredApiCodec
+{
+ public:
+  WiredApiCodec(){};
+  ~WiredApiCodec(){};
+
+  /*struct use for each codec to decode*/
+  void*			codecstruct;
+
+  /*init codec and looking for it formal*/
+  virtual void		init(list<s_LibInfo> &Info) = 0;
+
+  /*encode and decode functions*/
+  virtual int		encode(float** pcm) = 0;
+  virtual int		decode(const string filename, t_Pcm *pcm) = 0;
+};
+
+
+typedef struct		s_WLib
+{
+  list<t_LibInfo>      	Info;
+  WiredApiCodec*       	Codec;
+  t_Pcm			*pcm;
+}			t_WLib;
+
+#endif
+
