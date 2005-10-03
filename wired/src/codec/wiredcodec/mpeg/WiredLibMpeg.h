636b1eb9f8e95cca3eb0a99fc18223c5167f578d
nicolas-robin <nicolas-robin@users.noreply.github.com>
2005-10-03 15:04:35 +0000

    dynamic call to libmpeg3
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@462 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- wired/src/codec/wiredcodec/mpeg/WiredLibMpeg.h
+++ wired/src/codec/wiredcodec/mpeg/WiredLibMpeg.h
@@ -1,42 +1,67 @@
 #ifndef __WIREDLIBMPEG_H__
 #define __WIREDLIBMPEG_H__
 
-#include	"../WiredApiCodec.h"
-
 #include	<iostream>
 #include	<dlfcn.h>
+#include	"libmpeg3.h"
+#include	"../WiredApiCodec.h"
 
-#define		MPEG3_EXTENTION	"mp3\tMPEG-1 layer III (*.mp3)"
-#define		MPEG2_EXTENTION	"mp2\tMPEG-1 layer II (*.mp2)"
-#define		AC3_EXTENTION	"ac3\tAC3 (*.ac3)"
-#define		SO_NAME			"libmpeg3.so"
+#define		MPEG3_EXTENTION			"mp3\tMPEG-1 layer III (*.mp3)"
+#define		MPEG2_EXTENTION			"mp2\tMPEG-1 layer II (*.mp2)"
+#define		AC3_EXTENTION			"ac3\tAC3 (*.ac3)"
+#define		SO_NAME					"libmpeg3.so"
 
+#define		MPEG3_CHECK_SIG			"mpeg3_check_sig"
+#define		MPEG3_OPEN				"mpeg3_open"
+#define		MPEG3_TOTAL_AS_STREAM	"mpeg3_total_astreams"
+#define		MPEG3_AUDIO_CHANNELS	"mpeg3_audio_channels"
+#define		MPEG3_AUDIO_SAMPLES		"mpeg3_audio_samples"
+#define		MPEG3_SAMPLE_RATE		"mpeg3_sample_rate"
+#define		MPEG3_READ_AUDIO		"mpeg3_read_audio"
+#define		MPEG3_SEEK_BYTE			"mpeg3_seek_byte"
+#define		MPEG3_CLOSE				"mpeg3_close"
 
-bool test_decoders(t_Pcm *pcmOriginalPcm);
+typedef int			(*t_mpeg3_check_sig)		(char *path);
+typedef mpeg3_t* 	(*t_mpeg3_open)				(char *path);
+typedef int			(*t_mpeg3_total_astreams)	(mpeg3_t *file);
+typedef int			(*t_mpeg3_audio_channels)	(mpeg3_t *file, int stream);
+typedef long		(*t_mpeg3_audio_samples)	(mpeg3_t *file, int stream);
+typedef int			(*t_mpeg3_sample_rate)		(mpeg3_t *file, int stream);
+typedef int			(*t_mpeg3_read_audio)		(mpeg3_t *file, float *output_f, short *output_i, 
+  												int channel, long samples, int stream);
+typedef int			(*t_mpeg3_seek_byte)		(mpeg3_t *file, int64_t byte);
+typedef int			(*t_mpeg3_close)			(mpeg3_t *file);
 
 class   WiredLibMpeg: public WiredApiCodec
 {
  public:
 
   WiredLibMpeg(){std::cout << "[WIRED_MPEG_CODEC] Mpeg child created" << std::endl;}
-  ~WiredLibMpeg(){}
   WiredLibMpeg(const WiredLibMpeg& copy){*this = copy;};
+  ~WiredLibMpeg(){dlclose(handle);};
 
-  /* Inits codec */
-  void				init(list<s_LibInfo> &Info);
+  void				init(list<t_LibInfo> &Info);				/* Inits codec */
+  int				encode(float** pcm);						/* Encode methodes */
+  int				decode(const char *path, t_Pcm *pcm);		/* Decode methodes */
+  bool				canDecode(const char* path);				/* Checks format */
 
-  /* Encode and decode methodes */
-  int				encode(float** pcm);
-  int				decode(const char *path, t_Pcm *pcm);
-  
-  /* Checks format */
-  bool				canDecode(const char* path);
-
-  /* Operators */
   WiredLibMpeg		operator=(const WiredLibMpeg& right);
   
  private:
-  void			*handle;
+  void						*handle;
+  t_mpeg3_check_sig			mpeg3_check_sig_func;
+  t_mpeg3_open				mpeg3_open_func;
+  t_mpeg3_total_astreams	mpeg3_total_astreams_func;
+  t_mpeg3_audio_channels	mpeg3_audio_channels_func;
+  t_mpeg3_audio_samples		mpeg3_audio_samples_func;
+  t_mpeg3_sample_rate		mpeg3_sample_rate_func;
+  t_mpeg3_read_audio		mpeg3_read_audio_func;
+  t_mpeg3_seek_byte			mpeg3_seek_byte_func;
+  t_mpeg3_close				mpeg3_close_func;
+  
+  void				InitAccesLib();
+  void				fillLibInfo(t_LibInfo& info, char *extension);
+  void				mergeChannels(float* leftChan, float* rightChan, float* dst, int totalLen);
 };
 
 extern "C" WiredApiCodec  *const WiredCodecConstruct();
