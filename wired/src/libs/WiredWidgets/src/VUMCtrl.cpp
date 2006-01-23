469d7a9b71072494b7f3fadc600b13e8de747260
pj-leger <pj-leger@users.noreply.github.com>
2006-01-23 17:17:39 +0000

    fixed some bug : moving rack crashed. So destructor is now empty...
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@569 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- wired/src/libs/WiredWidgets/src/VUMCtrl.cpp
+++ wired/src/libs/WiredWidgets/src/VUMCtrl.cpp
@@ -128,10 +128,10 @@ int VUMCtrl::GetValue()
 
 VUMCtrl::~VUMCtrl()
 {
-	if (img_o)
-		delete img_o;
-	if (img_r)
-		delete img_r;
-	if (img_g)
-		delete img_g;
+//	if (img_o)
+//		delete img_o;
+//	if (img_r)
+//		delete img_r;
+//	if (img_g)
+//		delete img_g;
 }
