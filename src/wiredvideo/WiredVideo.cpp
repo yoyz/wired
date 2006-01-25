c6e65b3650efb760ee1282a7837a571b0915e668
nicolas-robin <nicolas-robin@users.noreply.github.com>
2006-01-25 17:44:42 +0000

    video added
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk/wired@584 6f5cd6f6-390f-0410-abd1-b47fe60d5253

---
+++ src/wiredvideo/WiredVideo.cpp
@@ -0,0 +1,80 @@
+#include	"WiredVideo.h"
+#include	"wx/wx.h"
+#include	<unistd.h>
+
+WiredVideo::WiredVideo()
+{
+	videoFilePath = "";
+	asFile = false;
+	isDisplayed = false;
+}
+
+WiredVideo::~WiredVideo()
+{
+	delete mplayer;
+}
+
+int		WiredVideo::InitMplayer()
+{
+	mplayer = new WiredMplayer();
+}
+
+int		WiredVideo::OpenFile(const std::string& path)
+{
+	wxFileDialog	*dlg = new wxFileDialog(0, "[WIREDVIDEO] Loading video file", path, "", VIDEO_FILE_FILTER, wxOPEN);
+
+  if (dlg->ShowModal() == wxID_OK)
+	{
+		if (videoFilePath != "") CloseFile();
+		videoFilePath = dlg->GetPath();
+		InitMplayer();
+		asFile = true;
+  }
+	DisplayVideoFrame();
+}
+
+int		WiredVideo::PlayFile()
+{
+	if (asFile == false) return 0;
+	std::cout << "[WIREDVIDEO] Playing video file" << std::endl;
+	return mplayer->PlayFile();
+}
+
+int		WiredVideo::StopFile()
+{
+	if (asFile == false) return 0;
+	std::cout << "[WIREDVIDEO] Video file stopped" << std::endl;
+	return mplayer->StopFile();
+}
+
+int		WiredVideo::PauseFile()
+{
+	if (asFile == false) return 0;
+	std::cout << "[WIREDVIDEO] Video file paused" << std::endl;
+	return mplayer->PauseFile();
+}
+
+int		WiredVideo::CloseFile()
+{
+	if (asFile == false) return 0;
+	isDisplayed = false;
+	std::cout << "[WIREDVIDEO] Closing video file" << std::endl;
+	videoFilePath = "";
+	asFile = false;
+	return mplayer->CloseFile();
+}
+
+int		WiredVideo::SeekFile(eSeekMethod seekMethod, double position)
+{
+	if (asFile == false) return 0;
+	std::cout << "[WIREDVIDEO] Seeking video file with method " << seekMethod << " at " << position << std::endl;
+	return mplayer->SeekFile(seekMethod, position);
+}
+
+bool	WiredVideo::DisplayVideoFrame()
+{
+	if (asFile == false) return 0;
+	isDisplayed = mplayer->DisplayVideoFrame(videoFilePath);
+	std::cout << "[WIREDVIDEO] Displaying video file" << std::endl;
+	return isDisplayed;
+}
