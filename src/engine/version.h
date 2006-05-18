77ec26c3e7b0dbeb2e0931b2ddca0b130f666ba0
hdoomsday <hdoomsday@users.noreply.github.com>
2006-05-18 18:22:40 +0000

    change version to 0.2.2
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk/wired@823 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- src/engine/version.h
+++ src/engine/version.h
@@ -1,15 +1,12 @@
 // Copyright (C) 2004-2006 by Wired Team
 // Under the GNU General Public License Version 2, June 1991
 
-// Copyright (C) 2004-2006 by Wired Team
-// Under the GNU General Public License
-
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
 #define WIRED_NAME	(wxT("Wired"))
-#define WIRED_TITLE	(wxT("Wired 0.2.1"))
-#define WIRED_VERSION	(wxT("0.2.1"))
+#define WIRED_TITLE	(wxT("Wired 0.2.2"))
+#define WIRED_VERSION	(wxT("0.2.2"))
 
 #define WIRED_BUGS	(wxT("http://bloodshed.net/wired/bugs"))
 #define WIRED_FORUMS	(wxT("http://bloodshed.net/wired/forums"))
