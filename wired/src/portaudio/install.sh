d4cc6d5923f6914620a0883645f4879170968d33
benouc <benouc@users.noreply.github.com>
2006-09-18 22:01:51 +0000

    Importing a recent snapshot of PortaudioV19
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@949 6f5cd6f6-390f-0410-abd1-b47fe60d5253

---
+++ wired/src/portaudio/install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+###
+### classic bootstrap stuff
+###
+set -x
+
+###
+### cleanning part
+###
+#  remove autotools stuff
+rm -rf config
+rm -f aclocal.m4 configure config.log config.status
+rm -rf autom4te*.cache
+# remove libtool stuff
+rm -f libtool
+# remove gettext stuff
+rm -f ABOUT-NLS
+rm -rf intl
+
+
+###
+### configuration part
+###
+# create the config directory
+mkdir config
+mkdir intl
+
+# execute autotools cmds
+autopoint -f				&& \
+aclocal --force				&& \
+libtoolize --force -c			&& \
+autoconf				&& \
+automake -a -f -c
+
+
+# Shut up
+set +x
