a7d847132b189934489f2e694e601ff991651279
hdoomsday <hdoomsday@users.noreply.github.com>
2006-10-07 12:57:34 +0000

    done the first version of plugins interface with the core. "WiredCorePlugins" renamed from HostCallback.
    git-svn-id: https://svn.code.sf.net/p/wired/code/branches/api-v2@1038 6f5cd6f6-390f-0410-abd1-b47fe60d5253

---
+++ wired/src/redist/WiredCorePlugins.cpp
@@ -0,0 +1,176 @@
+#include "WiredCorePlugins.h"
+
+WiredCorePlugins::WiredCorePlugins()
+{
+
+}
+
+WiredCorePlugins::~WiredCorePlugins()
+{
+
+}
+
+void		WiredCorePlugins::SendMouseEvent(Plugin *plug, wxMouseEvent *event)
+{
+  RackPanel->HandleMouseEvent(plug, event);
+}
+
+void		WiredCorePlugins::SendKeyEvent(Plugin *plug, wxKeyEvent *event)
+{
+  RackPanel->HandleKeyEvent(plug, event);
+}
+
+void		WiredCorePlugins::SendPaintEvent(Plugin *plug, wxPaintEvent *event)
+{
+  RackPanel->HandlePaintEvent(plug, event);
+}
+
+void		WiredCorePlugins::AddUpdatePlugin(Plugin *plug)
+{
+  MainWin->AddUpdatePlugin(plug);
+}
+
+wxString	WiredCorePlugins::GetProductName()
+{
+  return (wxT(WIRED_NAME));
+}
+
+wxString	WiredCorePlugins::GetProductVersion()
+{
+  return (wxT(WIRED_VERSION));
+}
+
+// long		WiredCorePlugins::GetBufferSize()
+// {
+
+// }
+
+// double		WiredCorePlugins::GetSamplingRate()
+// {
+
+// }
+
+float		WiredCorePlugins::GetBPM()
+{
+  return (Seq->BPM);
+}
+
+int		WiredCorePlugins::GetSigDenominator()
+{
+  return (Seq->SigDenominator);
+}
+
+int		WiredCorePlugins::GetSigNumerator()
+{
+  return (Seq->SigNumerator);
+}
+
+long		WiredCorePlugins::GetCurAudioPos()
+{
+  return (Seq->CurAudioPos);
+}
+
+double		WiredCorePlugins::GetSamplesPerMeasure()
+{
+  return (Seq->SamplesPerMeasure);
+}
+
+double		WiredCorePlugins::GetMeasurePerSample()
+{
+  return (Seq->MeasurePerSample);
+}
+
+double		WiredCorePlugins::GetCurrentPos()
+{
+  return (Seq->CurrentPos);
+}
+
+wxString	WiredCorePlugins::OpenFileLoader(wxString& title,
+						 std::vector<wxString>& extensions,
+						 bool akai)
+{
+  wxString	selected;
+  FileLoader*	dlg = new FileLoader(MainWin, MainWin_FileLoader,
+				     title,
+				     akai,
+				     false,
+				     extensions);
+
+  if (dlg->ShowModal() == wxID_OK)
+    selected = dlg->GetSelectedFile();
+  dlg->Destroy();
+}
+
+wxString	WiredCorePlugins::SaveFileLoader(wxString& title,
+						 std::vector<wxString>& extensions)
+{
+  wxString	selected;
+  FileLoader*	dlg = new FileLoader(MainWin, MainWin_FileLoader,
+				     title,
+				     false,
+				     true,
+				     extensions);
+
+  if (dlg->ShowModal() == wxID_OK)
+    selected = dlg->GetSelectedFile();
+  dlg->Destroy();
+}
+
+void		WiredCorePlugins::SendHelp(wxString& value)
+{
+  if (HelpWin->IsShown())
+    HelpWin->Help->Load_Text(value);
+}
+
+void		WiredCorePlugins::ShowOptionalView(WiredPlugin *plug)
+{
+  OptPanel->ShowPlug(plug);
+}
+
+void		WiredCorePlugins::CloseOptionalView(WiredPlugin *plug)
+{
+  OptPanel->ClosePlug(plug);
+
+}
+
+void		WiredCorePlugins::AddMidiPattern(WiredPlugin *plug,
+						 list<SeqCreateEvent*> midi)
+{
+  Seq->AddMidiPattern(midi, plug);
+}
+
+void		WiredCorePlugins::ShowMidiController(WiredPlugin *plug)
+{
+  MidiController*	Controller;
+
+  // lock mutex
+  MidiMutex.Lock();
+  Controller = new MidiController(plug)
+  MidiMutex.Unlock();
+
+  // get information from user and stock them
+  Controller->ShowModal();
+  _MidiType = Controller->Type;
+  _MidiController = Controller->Controller;
+  _MidiValue = Controller->Value;
+
+  // lock mutex, and delete object
+  MidiMutex.Lock();
+  delete Controller;
+  MidiMutex.Unlock();
+}
+
+int		WiredCorePlugins::GetLastMidiType()
+{
+  return (_MidiType);
+}
+
+int		WiredCorePlugins::GetLastMidiController()
+{
+  return (_MidiController);
+}
+
+int		WiredCorePlugins::GetLastMidiValue()
+{
+  return (_MidiValue);
+}
--- wired/src/redist/WiredCorePlugins.h
+++ wired/src/redist/WiredCorePlugins.h
@@ -1,10 +1,116 @@
+#ifndef __WIREDCOREPLUGINS_H__
+#define __WIREDCOREPLUGINS_H__
+
 class	WiredCorePlugins
 {
  private:
- public:
-  long		GetBufferSize();
-  double	GetSamplingRate();
-  float		GetBPM();
-  
+  int		_MidiType;
+  int		_MidiController;
+  int		_MidiValue;
 
+ public:
+
+  WiredCorePlugins::WiredCorePlugins();
+  WiredCorePlugins::~WiredCorePlugins();
+
+
+  /**
+   * Event related
+   */ 
+  void		SendMouseEvent(Plugin *plug, wxMouseEvent *event);
+
+
+  void		SendKeyEvent(Plugin *plug, wxKeyEvent *event);
+
+
+  void		SendPaintEvent(Plugin *plug, wxPaintEvent *event);
+
+
+  void		AddUpdatePlugin(Plugin *plug);
+
+
+  /**
+   * Basic info
+   */
+
+  wxString	GetProductName();
+
+
+  wxString	GetProductVersion();
+
+
+  // long		GetBufferSize();
+  // 
+
+  /**
+   * Audio info
+   */
+
+  // double		GetSamplingRate();
+  // 
+
+  float		GetBPM();
+
+
+  int		GetSigDenominator();
+
+
+  int		GetSigNumerator();
+
+
+  double	GetSamplesPerMeasure();
+
+
+  double	GetMeasurePerSample();
+
+
+  long		GetCurAudioPos();
+
+
+  double	GetCurrentPos();
+
+
+  /**
+   * Gui related
+   */
+
+  wxString	OpenFileLoader(wxString& title,
+			       std::vector<wxString>& extensions,
+			       bool akai);
+
+
+  wxString	SaveFileLoader(wxString& title,
+			       std::vector<wxString>& extensions);
+
+
+  void		SendHelp(wxString& value);
+
+
+  void		ShowOptionalView(WiredPlugin *plug);
+
+
+  void		CloseOptionalView(WiredPlugin *plug);
+
+  /**
+   * Midi related
+   */
+
+  void		AddMidiPattern(WiredPlugin *plug,
+			       list<SeqCreateEvent*> midi);
+
+
+  void		ShowMidiController(WiredPlugin *plug);
+
+
+  int		GetLastMidiType();
+
+
+  int		GetLastMidiController();
+
+
+  int		GetLastMidiValue();
+
+  
 };
+
+#endif // __WIREDCOREPLUGINS_H__
