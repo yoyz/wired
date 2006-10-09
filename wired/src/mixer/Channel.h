6e004ba7b4f97d645dee362552acd1dc0e1478d3
demzzz <demzzz@users.noreply.github.com>
2006-10-09 15:57:34 +0000

    Class commented.
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@1051 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- wired/src/mixer/Channel.h
+++ wired/src/mixer/Channel.h
@@ -10,49 +10,199 @@
 
 #define  NUM_BUFFERS  1
 
+
+/**
+ * The Channel class manages mono/stereo buffers,
+ * their associated volumes and allow some controls
+ * on them.
+*/
+
 class Channel
 {
  public:
+
+/**
+ * Ctor setting volumes for each channel
+*/
   Channel(bool stereo, bool visible = true);
+
+/**
+ * Copy Ctor
+*/
+
   Channel(const Channel& copy){*this = copy;}
   Channel	operator=(const Channel& right);
+
+/**
+ * DCtor cleaning all channels
+*/
+
   ~Channel();
 
+/**
+ * Dump all values
+ * \return void
+*/
   void		Dump();
 
+/**
+ * True for Stereo, false for mono
+*/
+
   bool		Stereo;
+
+/**
+ * Float volume for the left channel
+*/
+
   float		VolumeLeft;
+
+/**
+ * Float volume for the right channel
+*/
+
   float		VolumeRight;
- 
+
+/**
+ * Variable is set but currently unused
+*/
+
   long		InputNum;
-  
+
+/**
+ * Stereo buffer filling
+ * \param input a float*
+ * \param bytes a long
+ * \return void
+*/
+
   void		PushBuffer(float* input, long bytes);
 
+/**
+ * Mono Buffer filling
+ * \param a float **
+ * \return void
+*/
+
   void		PushBuffer(float **);
+
+/**
+ * Mono buffer filling
+ * \param a float *
+ * \return void
+*/
+
   void		PushBuffer(float *);
+
+/**
+ * Allocates memory for a given numbers of buffers
+ * \param unsigned int
+ * \return void
+*/
+
   void		AddBuffers(unsigned int);
+
+/**
+ * Returns a buffer at the given position int i
+ * \param i an int
+ * \return a buffer as a float*
+*/
+
   float		*PopBuffer(int i);
+
+/**
+ * Removes the first buffer of the vector
+ * \return void
+*/
+
   void		RemoveFirstBuffer();
+
+/**
+ * Delete all values in the buffer's vector
+ * \param renew a bool, if true, call AddBuffers(NUM_BUFFERS)
+ * after the full vector's buffers removal (default is true)
+ * \return void
+*/
+
   void		ClearAllBuffers(bool renew = true);
+
+/**
+ * Variable is set but currently unused
+*/
+
   wxString	Label;
+
+/**
+ * If Visible is true, buffers will be fully loaded
+ * else will be only allocated
+*/
+
   bool		Visible;
+
+/**
+ * float for left RMS
+*/
+
   float		Lrms;
+
+/**
+ * float for right RMS
+*/
+
   float		Rrms;
-  
+
+/**
+ * Where mono buffers are stored
+*/
+
   vector<float*> MonoBuffers;
+
+/**
+ * Where stereo buffers are stored
+*/
+
   vector<float**> StereoBuffers;
-  
+
+/**
+ * Represent a position in the MonoBuffers vector
+*/
+
   int		CurBuf;
-  
+
+/**
+ * Variable is set but currently unused
+*/
+
   bool		Filled;
-  
+
+/**
+ * Mute setter for left channel
+*/
+
   inline void	SetMuteLeft(bool muted)
     { MuteLeft = muted; };
+
+/**
+ * Mute setter for right channel
+*/
+
   inline void	SetMuteRight(bool muted)
     { MuteRight = muted; };
 
  private:
+
+/**
+ * True if the left channel is muted, else false
+ *
+*/
+
   bool		MuteLeft;
+
+/**
+ * True if the right channel is muted, else false
+ *
+*/
+
   bool		MuteRight;
 };
 
