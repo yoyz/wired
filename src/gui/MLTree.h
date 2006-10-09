bc419687f7073da563e9531121e6960ae67451b3
scoup <scoup@users.noreply.github.com>
2006-10-09 15:32:53 +0000

    commented classes MediaLibrary, MLTree, MixerGui
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk/wired@1049 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- src/gui/MLTree.h
+++ src/gui/MLTree.h
@@ -4,9 +4,7 @@
 #ifndef __MLTREE_H__
 #define __MLTREE_H__
 
-//#include <wx/wx.h>
 #include <vector>
-//#include <list>
 #include <map>
 #include <wx/treectrl.h>
 
@@ -16,6 +14,9 @@ using std::map;
 
 #define EXT_FILE		wxT("wired_exts.conf")
 
+/**
+ * The s_nodeInfo struct contains various informations about files
+*/
 struct				s_nodeInfo
 {
   wxString			label;
@@ -23,64 +24,276 @@ struct				s_nodeInfo
   wxString			length;
 };
 
+/**
+ * The MLTree class handle the tree of the MediaLibrary. It is derived from wxTreeCtrl
+ */
 class				MLTree : public wxTreeCtrl
 {
-
  public:
+/**
+ * Main constructor for class MLTree
+*/
   MLTree(wxWindow *dad, wxPoint p, wxSize s, long style);
+/**
+ * Main destructor for class MLTree
+*/
   ~MLTree();
 
  protected:
   friend class			MediaLibrary;
   friend class			MLTreeInfos;
 
+/**
+ * wxTreeCtrl to display the tree and the nodes
+*/
   wxTreeCtrl			*Tree;
+/**
+ * Boolean used to know if the media library nodes are collapsed or not
+*/
   bool				collapsed;
+/**
+ * wxTreeItemId root is the first node of the Tree. It contains the project
+ * name.
+*/
   wxTreeItemId			root;
+/**
+ * a wxString used by filters
+*/
   wxString			filters;
+/**
+ * A vector of wxStrings containing all the known sounds extentions
+*/
   vector<wxString>		Exts;
+/**
+ * A Map of wxTreeItemId and their corresponding struct which contains 
+ * various informations related to the item.
+*/
   map<wxTreeItemId, s_nodeInfo>	nodes;
+/**
+ * A wxPoint which contains the position of the mouse when using right click
+*/
   wxPoint			mouse_pos;
+/**
+ * A wxString that holds the name of the selected node
+*/
   wxString			selected;
+/**
+ * A wxTreeItemId used for the drag and drop feature
+*/
   wxTreeItemId		        item_begin;
+/**
+ * A wxTreeItemId used for the drag and drop feature
+*/
   wxTreeItemId		        item_to_drag;
+/**
+ * A wxArrayTreeItemsIds which contains multiple selection items ids
+*/
   wxArrayTreeItemIds		selection;
+/**
+ * An int holding the lenght number of the current selection
+*/
   int				selection_length;
+/**
+ * The method used to serialize the MediaLibrary Content
+ * \return a boolean
+*/
   bool				SaveML();
+/**
+ * The function IsTreeCollapsed is used to know if the nodes are expanded or
+ * not.
+ * \return a boolean set to true if the Tree is collapsed, else return false.
+*/
   bool				IsTreeCollapsed();
+/**
+ * The SetTreeCollapsed function is a seter used when clicking the expand or
+ * collapse button. It sets the collapsed variable to true.
+ * \return void
+*/
   void				SetTreeCollapsed();
+/**
+ * The SetTreeExpanded function is a seter used when clicking the expand or
+ * collapse button. It sets the collapsed variable to false.
+ * \return void
+*/
   void				SetTreeExpanded();
+/**
+ * The GetTreeItemIdFromLabel function is used to get the id of a node using 
+ * it's label.
+ * \param label a wxString containing the name of a node
+ * \return a wxTreeItemId corresponding to the selectioned node
+*/
   wxTreeItemId			GetTreeItemIdFromLabel(wxString label);
+/**
+ * The GetTreeItemStructFromId function is used to get the struct s_node_info
+ * associated to a node item using it's Id.
+ * \param ItemToFind a wxTreeItemId corresponding to the desired node
+ * \return a s_node_info struct correponding to the desired node
+*/
   s_nodeInfo			GetTreeItemStructFromId(wxTreeItemId ItemToFind);
+/**
+ * The getSelection return the label of the selected node
+ * \param flag, an int set to 1 or 0 to determine if the label is taken from
+ * the struct or from the tree
+ * \return a wxString containing the label of the selected node
+*/
   wxString			getSelection(int flag);
+/**
+ * The AddFile function adds a new node 
+ * \param ParentNode a wxTreeItemId corresponding to the parent node of the
+ * new node.
+ * \param wxString FileToAdd the full path to the file to add
+ * \param infosx a s_nodeInfo struct used to hold informations about the file
+ * \param expand, a boolean used to expand or not the new node
+ * \return void
+*/
   void				AddFile(wxTreeItemId ParentNode, wxString FileToAdd, s_nodeInfo infos, bool expand);
+/**
+ * The ExpandAll function sets all the existing nodes to the state expanded 
+ * \param Tree, a wxTreeCtrl which is the tree to expand
+ * \param id, a wxTreeItemId which is used for recursivity
+ * \param shouldExpand, a boolean used to expand or not the current node
+ * \param toLevel, an int which represent the level of expansion
+ * \return void
+*/
   void				ExpandAll(wxTreeCtrl *Tree, const wxTreeItemId& id, bool shouldExpand, int toLevel);
+/**
+ * The OnCollapse function is called when launching the Expand or Collapse 
+ * action.
+ * \return void
+*/
   void				OnCollapse();
+/**
+ * The LoadKnownExtentions function load the known extentions file types.
+ * \return void
+*/
   bool				LoadKnownExtentions();
+/**
+ * The AddIcon function add a visual icon to a node.
+ * \param images a wxImageList holding the available images
+ * \param icon a wxIcon
+ * \return void
+*/
   void				AddIcon(wxImageList *images, wxIcon icon);
+/**
+ * The OnAdd function is called by launching the Add File action, using the
+ * dedicated button. The file is added to the selected node or automaticaly 
+ * sorted if no node is selected
+ * \param FileToAdd a wxString which is the full path to the file to add
+ * \return void
+*/
   void				OnAdd(wxString FileToAdd);
+/**
+ * The OnRemove function remove the selected nodes
+ * \return void
+*/
   void				OnRemove();
+/**
+ * The OnCreateDir function is called when creating a new directory. It 
+ * create a new directory node.
+ * \return void
+*/
   void				OnCreateDir();
-  /////
+/**
+ * The OnInsert function is called when inserting the selected item into a
+ * track
+ * \return void
+*/
   void				OnInsert();
+/**
+ * The OnEdit function is called when launching the Edit function. It loads
+ * the file into the wired wav editor
+ * \return void
+*/
   void				OnEdit();
+/**
+ * The OnPreview function is called when lauching the preview action. It 
+ * starts playing the selected file
+ * \return void
+*/
   void				OnPreview();
+/**
+ * The DisplayInfos function is called when lauching the File Infos action.
+ * It open a tool tip containing various informations about the selected node
+ * \return void
+*/
   void				DisplayInfos();
-  /////
+/**
+ * The OnRightClick function is called when right clicking a node.
+ * \param event a wxMouseEvent& because this is the normal wx event handling
+ * \return void
+*/
   void				OnRightClick(wxMouseEvent &event);
+/**
+ * The OnContextMenu is not documented yet.
+ * \param event a wxMouseEvent& because this is the normal wx event handling
+ * \return void
+*/
   void				OnContextMenu(wxMouseEvent &WXUNUSED(event));
+/**
+ * The OnSelChange is called every time the user select a node from the tree
+ * \param event a wxTreeEvent& because this is the normal wx event handling
+ * \return void
+*/
   void				OnSelChange(wxTreeEvent &event);
+/**
+ * The BeginDrag function is called when the user starts to drag and drop a
+ * node containing a file
+ * \param event a wxTreeEvent& because this is the normal wx event handling
+ * \return void
+*/
   void				BeginDrag(wxTreeEvent &event);
+/**
+ * The EndDrag function is called when the user finishes to drag and drop a
+ * node containing a file
+ * \param event a wxTreeEvent& because this is the normal wx event handling
+ * \return void
+*/
   void				EndDrag(wxTreeEvent &event);
+/**
+ * The DisplayNodes is here for debug only
+ * \return void
+*/
   void				DisplayNodes();
+/**
+ * The SortNodes is used to sort the node depending on the user's choice
+ * \param selected, a wxString containing the name of the selected node
+ * \return void
+*/
   void				SortNodes(wxString selected);
+/**
+ * The OnCompareItems is used to compare two items
+ * \param item1, the first wxTreeItemId&
+ * \param item2, the second wxTreeItemId&
+ * \return an int set to 0 if true
+*/
   int				OnCompareItems(const wxTreeItemId& item1, const wxTreeItemId& item2);
+/**
+ * The Copy function returns a copy of a node
+ * \param item, the wxTreeItemId to copy
+ * \return a wxTreeItemId
+*/
   wxTreeItemId                  Copy(wxTreeItemId item);
+/**
+ * The DragAndDrop function is here for the drag and drop feature
+ * \param item, the wxTreeItemId representing the node to drag
+ * \return void
+*/
   void                          DragAndDrop(wxTreeItemId item);
+/**
+ * The OnLeftClick is called when left clicking a node
+ * \param event, a wxMouseEvent& because this is the normal event handling
+ * \return void
+*/
   void				OnLeftClick(wxMouseEvent &event);
+/**
+ * The declaration of the event table
+*/
   DECLARE_EVENT_TABLE()
 };
 
+/**
+ * enums for event handling
+*/
 enum
 {
   ML_ID_MENU_ADD = 20600,
--- src/gui/MediaLibrary.h
+++ src/gui/MediaLibrary.h
@@ -24,7 +24,6 @@ class					MLTree;
 #define MEDIALIBRARY_INSERTUP_IMG	wxT("ihm/toolbar/insert_up.png")
 #define MEDIALIBRARY_INSERTDO_IMG	wxT("ihm/toolbar/insert_up.png")
 
-
 #define MEDIALIBRARY_PREVIEWUP_IMG	wxT("ihm/toolbar/play.png")
 #define MEDIALIBRARY_PREVIEWDO_IMG	wxT("ihm/toolbar/play.png")
 #define MEDIALIBRARY_COLLAPSEUP_IMG	wxT("ihm/toolbar/collapse_up.png")
@@ -39,74 +38,269 @@ class					MLTree;
 #define MEDIALIBRARY_FILEFXUP_IMG	wxT("ihm/toolbar/file_fx_up.png")
 #define MEDIALIBRARY_FILEFXDO_IMG	wxT("ihm/toolbar/file_fx_up.png")
 
-
 #define ML_BACKGROUND			wxColour(142, 142, 169)
 #define ML_FOREGROUND			wxColour(142, 142, 155)
 #define NB_SORTSELECT_CHOICES		(4)
 #define DEFAULT_SORTSELECT_VALUE	wxT("Sort by...")
 
 
+/**
+ * The MediaLibrary class is the main class for the media library feature
+ * It is designed to build the interface dedicated to the medialibrary
+ * It is derived from wxPanel. It include the buttons and the tree
+*/
 class				MediaLibrary : public wxPanel
 {
  public:
+/**
+ * Default constructor for the class MediaLibrary
+*/
   MediaLibrary(wxWindow *parent,
 	       const wxPoint &pos = wxDefaultPosition,
 	       const wxSize &size = wxDefaultSize,
 	       long style = wxTAB_TRAVERSAL);
+/**
+ * Default destructor for the class MediaLibrary
+*/
   ~MediaLibrary();
 
-
  protected:
- 
-  friend class			MLTree;
 
+  friend class			MLTree;
+/**
+ * Boolean used to know if the media library panel is visible or unvisible
+*/
   bool				visible;
+/**
+ * Boolean used to know if the media library panel is floating or docked
+*/
   bool				floating;
+/**
+ * wxToolBar containing the butons located on top of the medialibrary panel
+*/
   wxToolBar			*TopToolbar;
+/**
+ * wxToolBar containing the butons located on bottom of the medialibrary
+ * panel
+*/
   wxToolBar			*BottomToolbar;
+/**
+ * wxToolBar containing the butons dedicated to the filters of the filetypes
+ * This wxToolBar is located under the BottomToolbar
+*/
   wxToolBar			*FiltersToolbar;
+/**
+ * wxBoxSizer containing all the previously defined wxToolBar
+*/
   wxBoxSizer			*TopSizer;
-  // A effacer ?
-  //  wxBoxSizer			*TreeSizer;
-  //  wxBoxSizer			*BottomSizer;
-  ///////////
+/**
+ * wxComboBox designed to hold the rolling menu offering sorting
+ * possibilities
+*/
   wxComboBox			*SortSelect;
-  wxTextCtrl			*mlTextCtrl;
-  wxMenu			*PopMenu;
+/**
+ * MLTree derived from wxTreeCtrl. Contains the tree of the Media Library
+*/
   MLTree			*MLTreeView;
+/**
+ * FileConversion for converting files to the used samplerate before using
+ * them in the Sequencer
+*/
   FileConversion		*FileConverter;
-  
+
  public:
+/**
+ * The SetFileConverter, used to define a fileconverter in the medialibrary
+ * \param FileConv an *FileConversion object
+ * \return void
+*/
   void				SetFileConverter(FileConversion *FileConv);
+/**
+ * The OnSize function is called by an event every time the media library
+ * panel is resized by the user. It change the size of the Tree and the
+ * position of the buttons.
+ * \param event a &wxSizeEvent because it is the normal wxSize event handling
+ * \return void
+*/
   void				OnSize(wxSizeEvent &event);
+/**
+ * The OnEdit function is called by launching the Edit action, using the
+ * dedicated button or the contextual menu. It insert the file into a track
+ * and then into the Wav editor
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnEdit(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnInsert function is called by launching the Insert action, using the
+ * dedicated button or the contextual menu. It insert the file into a track
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnInsert(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnPreview function is called by launching the Preview action, using
+ * the dedicated button or the contextual menu. It launch a preview of the
+ * file selected into the Media Library Tree
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnPreview(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnAdd function is called by launching the Add File action, using the
+ * dedicated button. It launch the wired file browser allowing the user to
+ * select a file. When validating, the file is added to the selected node
+ * or automaticaly sorted if no node is selected
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnAdd(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnRemove function is called by launching the Delete action, using the
+ * dedicated button or the contextual menu. It delete the selected file
+ * from the Media Library Tree
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnRemove(wxCommandEvent &WXUNUSED(event));
+
+/**
+ * The OnCollapse function is called by launching the Remove action using
+ * the dedicated button. It collapse or expand all existing nodes from the
+ * MediaLibrary Tree, depending of the state of the tree
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnCollapse(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnSortToggle function is called when changing the selection of the
+ * SortToggle variable. It retreive the new selection before calling the
+ * SortNodes(wxString MLselected) function from the MLTree class.
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnSortToggle(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnFilterAudio function is called by launching the Filter Audio action
+ * using the dedicated button. It will browse all existing nodes in the Tree
+ * and show or hide the audio files.
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnFilterAudio(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnFilterMIDI function is called by launching the Filter MIDI action
+ * using the dedicated button. It will browse all existing nodes in the Tree
+ * and show or hide the MIDI files.
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnFilterMIDI(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnFilterVideo function is called by launching the Filter Video action
+ * using the dedicated button. It will browse all existing nodes in the Tree
+ * and show or hide the video files.
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnFilterVideo(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnFilterEffects function is called by launching the Filter Effects 
+ * action using the dedicated button. It will browse all existing nodes in 
+ * the Tree and show or hide the Effects files.
+ * \param event an unused wxCommandEvent because it is the normal wx event
+ * handling
+ * \return void
+*/
   void				OnFilterEffects(wxCommandEvent &WXUNUSED(event));
+/**
+ * The OnRightClick function is called by using the mouse right click on a
+ * MLTree node. It displays a menu with possibles actions depending on the
+ * kind of item selected.
+ * \param event a wxMouseEvent because it is the normal wx mouse event
+ * handling
+ * \return void
+*/
   void				OnRightClick(wxMouseEvent &event);
+/**
+ * The OnLeftClick function is called by using the mouse left click on a
+ * MLTree node. 
+ * \param event a wxMouseEvent because it is the normal wx mouse event
+ * handling
+ * \return void
+*/
   void				OnLeftClick(wxMouseEvent &event);
-  void				ShowPopup(wxPoint pos);
- 
+/**
+ * The ShowPopup function is called 
+ * \param event a wxMouseEvent because it is the normal wx mouse event
+ * handling
+ * \return void
+*/
+//  void				ShowPopup(wxPoint pos);
+
+/**
+ * The IsVisible function is used to test if the MediaLibrary is visible
+ * or hidden.
+ \return a boolean. true if the MediaLibrary is visible, else return false
+*/
   bool				IsVisible();
+/**
+ * The SetInVisible function is a seter called when seting the MediaLibrary
+ * panel to invisible. It set the boolean visible to false.
+ \return void
+*/
   void				SetInvisible();
+/**
+ * The SetVisible function is a seter called when seting the MediaLibrary
+ * panel to visible. It set the boolean visible to true.
+ \return void
+*/
   void				SetVisible();
+/**
+ * The IsFloating function is used to test if the MediaLibrary is floating
+ * or docked.
+ \return a boolean. true if the MediaLibrary is floating, else return false
+*/
   bool			        IsFloating();
+/**
+ * The SetFloating function is a seter called when seting the MediaLibrary
+ * panel to floating. It set the boolean floating to true.
+ \return void
+*/
   void				SetFloating();
+/**
+ * The SetDocked function is a seter called when seting the MediaLibrary
+ * panel to docked. It set the booelan floating to false.
+ \return void
+*/
   void				SetDocked();
 
+/**
+ * Event table where are enumerated all the events associated to the 
+ * MediaLibrary.
+*/
   DECLARE_EVENT_TABLE()
 };
 
-
+/**
+ * Struct s_combo_choice containing the list of possible choices in the 
+ * select button dedicated to the filetypes sorting.
+*/
 extern const struct s_combo_choice	SortSelectChoices[NB_SORTSELECT_CHOICES + 1];
 
+/**
+ * The enum table
+*/
 enum
 {
    MediaLibrary_Add = 1,
