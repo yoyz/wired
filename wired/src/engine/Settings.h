fe5874782b6a76f669908103ad83fb5cca9d0954
carres_l <carres_l@users.noreply.github.com>
2006-10-13 10:26:22 +0000

    Code Documentation : Settings.h
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@1059 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- wired/src/engine/Settings.h
+++ wired/src/engine/Settings.h
@@ -18,67 +18,211 @@ using namespace std;
 
 #define WIRED_CONF_VERSION	wxT("1")
 
+/**
+ * Setting for wired.
+ * Set Default,read and save channels,Load and save the settings.
+ */
 class Settings
 {
  public:
+
+  /**
+   * Default constructor.
+   * checking if config file existed.
+   */
   Settings();
+
+  /**
+   * The main destructor.
+   */
   ~Settings();
 
-  // Options
+  /**
+   * Quickly render waveforms.
+   * The default setting is false.
+   */
   bool		QuickWaveRender;
+
+  /**
+   * Render waveforms in db mode.
+   * The default setting is false.
+   */ 
   bool		dbWaveRender;
+
+  /**
+   * Undo Redo maximum depth.
+   * The default value is DEFAULT_MAXUNDOREDODEPTH (20).
+   */
   long		maxUndoRedoDepth;
 
+  /**
+   * the default value is -1.
+   */
   double	OutputLatency;
+
+  /**
+   * the default value is -1.
+   */
   double	InputLatency;
   
+  /**
+   * Setting the sample rate.
+   * the default value is DEFAULT_SAMPLE_RATE_INT.
+   */
   long		SampleRate;
+
+  /**
+   * how many samples per buffer.
+   * the default value is DEFAULT_SAMPLES_PER_BUFFER.
+   */
   long		SamplesPerBuffer;
+
+  /** 
+   * The sample format.
+   * The default value is DEFAULT_SAMPLE_FORMAT.
+   */
   long		SampleFormat; 
 
-  // string from conf file
+  /**
+   * The string of output system from config file.
+   */
   wxString	OutputSystemStr;
+
+  /**
+   * The string of input system from config file.
+   */
   wxString	InputSystemStr;
 
+  /**
+   * The string of output device from config file.
+   */
   wxString	OutputDeviceStr;
+ 
+  /**
+   * The string of input device from config file.
+   */
   wxString	InputDeviceStr;
 
-  // id not in conf file
+  /**
+   * The default value of output system id is 0.
+   */
   long		OutputSystemId;
+
+  /**
+   * The default value of input system id is 0.
+   */
   long		InputSystemId;
 
+  /**
+   * The default value of output device id is 0.
+   */
   long		OutputDeviceId;
+
+  /**
+   * The default value of input device id is 0.
+   */
   long		InputDeviceId;
 
+  /**
+   * Output channels.
+   */
   vector<long>	OutputChannels;
+
+  /**
+   * Input channels.
+   */
   vector<long>	InputChannels;
+
+  /**
+   * The midi devices.
+   */
   vector<long>	MidiIn;
 
+  /**
+   * Setting a path to put a config file of plugin.
+   */
   wxString	PlugConfFile;
+
+  /**
+   * Setting a directory of config file.
+   */
   wxString	ConfDir;
+ 
+  /**
+   * Setting a directory of data.
+   */
   wxString	DataDir;
+
+  /** 
+   * Setting a working directory.
+   */
   wxString	WorkingDir;
 
+  /**
+   * Loading the settings.
+   * setting the path and checking if it's wired config version.
+   */
   void		Load();
+
+  /**
+   * Delete deprecated entries and saving all settings.
+   */
   void		Save();
 
+  /** 
+   * Checking if it's first launch.
+   * \return returns a bool,if it's first launch,then it returns TRUE.
+   * else it returns FALSE.
+   */
   inline bool	IsFirstLaunch() { return(FirstLaunch); };
+
+  /**
+   * Checking if the configuration is deprecated.
+   * \return returns a bool,if it's deprecated then it returns TRUE.
+   * else it returns FALSE.
+   */
   inline bool	ConfIsDeprecated() { return(ConfDeprecated); };
 
  protected:
   wxConfig	*conf;
 
  private:
+
+  /**
+   * Default value is false.
+   */
   bool		FirstLaunch;
+
+  /**
+   * Default value is false.
+   */
   bool		ConfDeprecated;
 
+  /** 
+   * Setting all defaults.
+   */
   void		SetDefault();
-
+  
+  /**
+   * Saving channels.
+   * \param Group a wxString,the name of title.
+   * \param list a vector<long>&,the list of channels.
+   */
   void		SaveChannels(wxString Group, vector<long>& list);
+
+  /**
+   * Reading channels.
+   * \param Group a wxString,the name of title.
+   * \param list a vector<long>&,the list of channels.
+   */
   void		ReadChannels(wxString Group, vector<long>& list);
 
+  /**
+   * Delete deprecated entries.
+   */
   void		DeleteDeprecatedEntries();
 };
+
 
 extern Settings *WiredSettings;
 
