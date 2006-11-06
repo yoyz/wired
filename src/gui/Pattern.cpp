ac69bf07657225d87b15d9451f3d67f5f4cdcfe9
n3os <n3os@users.noreply.github.com>
2006-11-06 11:52:12 +0000

    Improve drag and drop when you change the vertical zoom value
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk/wired@1164 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- src/gui/Pattern.cpp
+++ src/gui/Pattern.cpp
@@ -214,7 +214,7 @@ void					Pattern::OnMotion(wxMouseEvent &e)
 	{
 	  if (SeqPanel->SelectedItems.size() == 1)
 	    {
-	      trackto = TrackIndex + floor((e.GetPosition().y  * SeqPanel->VertZoomFactor) / TRACK_HEIGHT);
+	      trackto = TrackIndex + floor((e.GetPosition().y ) / (TRACK_HEIGHT * SeqPanel->VertZoomFactor));
 	      if (SeqPanel->IsAudioTrack(TrackIndex) == SeqPanel->IsAudioTrack(trackto))
 		{
 		  SeqPanel->ChangeMouseCursor(wxCursor(wxCURSOR_HAND));
