d460596d849edd54643530aa306c8de1e3ceac74
hdoomsday <hdoomsday@users.noreply.github.com>
2007-01-26 10:46:19 +0000

    added --enable-pormidi flag (enabled by default) change version of trunk to 0.4
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk/wired@1282 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- src/engine/version.h
+++ src/engine/version.h
@@ -5,10 +5,10 @@
 #define __VERSION_H__
 
 #define WIRED_NAME	(wxT("Wired"))
-#define WIRED_TITLE	(wxT("Wired 0.4 alpha"))
-#define WIRED_VERSION	(wxT("0.4 alpha"))
+#define WIRED_TITLE	(wxT("Wired 0.4"))
+#define WIRED_VERSION	(wxT("0.4"))
 
 #define WIRED_BUGS	(wxT("http://sourceforge.net/projects/wired"))
-#define WIRED_FORUMS	(wxT("http://wired.sourceforge.net"))
+#define WIRED_FORUMS	(wxT("http://wired.epitech.net/userforum"))
 
 #endif //  __VERSION_H__
