4daf5ba3e493d991fd5a99cf366c7e9c4046be0d
oxesoft <oxesoft@users.noreply.github.com>
2007-06-09 01:10:21 +0000

    Removed remaining include relative paths
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@1438 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- wired/src/libs/WiredAkai/src/list.cpp
+++ wired/src/libs/WiredAkai/src/list.cpp
@@ -16,7 +16,7 @@
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-#include "../include/list.h"
+#include "list.h"
 #include <stdlib.h>
 
 #define CREATE_NODE(node, e, n) node = (t_list *)malloc(sizeof(t_list));\
