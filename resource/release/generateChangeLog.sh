3bed42fdcd2d7a1b48d5a0131f7395641442b852
kiketloule <kiketloule@users.noreply.github.com>
2009-04-05 11:35:22 +0000

    [Release] Cleaner xsl stylesheet, included svn2cl stylesheet
    git-svn-id: https://svn.code.sf.net/p/wired/code/trunk@1536 6f5cd6f6-390f-0410-abd1-b47fe60d5253

--- resource/release/generateChangeLog.sh
+++ resource/release/generateChangeLog.sh
@@ -7,17 +7,27 @@ temp_file="./generateChangeLog.tmp"
 url_svn_tags=${url_svn}"/tags"
 url_svn_trunk=${url_svn}"/trunk"
 
+#Echoes the last changed revision for a given svn url
+getLastChangedRevForUrl() {
+  url=${1}
+
+  last_rev=$( svn info ${url} | grep "Last Changed Rev" | grep -o -e "[0-9]\+$" )
+
+  echo ${last_rev}
+}
+
+
 #On prend l'avant dernièr (n-1 ligne car on est censé avoir déjà taggé.
 echo "Getting last release tag..."
 last_tag=$( svn ls ${url_svn_tags} | tail -1 ) 
 
 #On récupère le numéro de la derniere revision de ce tag
 echo "Getting last revision for tag [${last_tag}]..."
-last_revision=$( svn info ${url_svn_tags}/${last_tag} | grep "Last Changed Rev" | grep -o -e "[0-9]\+$" );
+last_revision=$( getLastChangedRevForUrl ${url_svn_tags}/${last_tag} )
 
 #On récupère le numéro de la revision courante
 echo "Getting current revision..."
-current_revision=$( svn info ${url_svn}/"trunk" | grep "Last Changed Rev" | grep -o -e "[0-9]\+$" );
+current_revision=$( getLastChangedRevForUrl ${url_svn}/"trunk" );
 
 #On fait le bordel pour écrire le changelog au début du fichier
 echo "Adding logs between revision [${last_revision}] and HEAD"
@@ -31,7 +41,7 @@ echo "------------------------------------------------------------------------" 
 echo "" >> ${output_file}
 #On ajoute le dernier changelog
 #Le dernier grep est un peu une feinte mais bon...
-svn log --xml -rHEAD:${last_revision} ${url_svn_trunk} | grep -v "^$" | xsltproc ./transform.xsl - | grep -v "<?xml" >> ${output_file}
+svn log --xml -rHEAD:${last_revision} ${url_svn_trunk} | xsltproc ./transform.xsl - | grep -v -e "<?xml" >> ${output_file}
 #On remet l'ancien changelog derrière
 cat ${temp_file} >> ${output_file}
 rm ${temp_file}
--- resource/release/transform.xsl
+++ resource/release/transform.xsl
@@ -1,7 +1,26 @@
+
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-<xsl:template match="/">
-<xsl:for-each select="log/ logentry">
-- <xsl:value-of select="msg" />
-</xsl:for-each>
-</xsl:template>
+ <xsl:output
+	method="text"
+	omit-xml-declaration="yes"
+	indent="no" />
+
+ <!-- include Arthur de Jong's svn2cl -->
+ <xsl:include href="svn2cl.xsl" />
+
+  <xsl:template match="/log">
+   <xsl:for-each select="logentry">
+    <xsl:text>- </xsl:text>
+    <xsl:call-template name="trim-newln">
+     <xsl:with-param name="txt" select="msg" />
+    </xsl:call-template>
+    <!-- A new line -->
+    <xsl:text>&#10;</xsl:text>
+   </xsl:for-each>
+   <!-- A new line -->
+   <xsl:text>&#10;</xsl:text>
+  </xsl:template>
+
+
 </xsl:stylesheet>
+
